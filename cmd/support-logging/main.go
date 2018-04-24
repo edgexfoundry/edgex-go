aa970b641472af2c88fe64ee9539597918a4c9c0
Trevor Conn <trevor_conn@dell.com>
2018-04-23 19:06:25 -0500

    Adding command line params to docker entrypoints
    1.) Integrated supported cmd line params into docker files for
        core-command, core-data, core-metadata, support-logging.
    2.) Modified handling of --consul cmds line param. Since it is
        effectively a boolean, removed necessity to specify --consul=y
    3.) In docker files, configuration-docker.toml cannot be renamed
        to configuration.toml or else the file will not be found.
        This is due to the --profile cmd line value handling.
    
    Signed-off-by: Trevor Conn <trevor_conn@dell.com>

--- cmd/support-logging/main.go
+++ cmd/support-logging/main.go
@@ -25,14 +25,14 @@ import (
 var loggingClient logger.LoggingClient
 
 func main() {
-	var useConsul, useProfile string
+	var useConsul bool
+	var useProfile string
 
-	flag.StringVar(&useConsul, "consul", "n", "Should the service use consul?")
-	flag.StringVar(&useConsul, "c", "n", "Should the service use consul?")
+	flag.BoolVar(&useConsul, "consul", false, "Indicates the service should use consul.")
+	flag.BoolVar(&useConsul, "c", false, "Indicates the service should use consul.")
 	flag.StringVar(&useProfile, "profile", "default", "Specify a profile other than default.")
 	flag.StringVar(&useProfile, "p", "default", "Specify a profile other than default.")
 	flag.Usage = usage.HelpCallback
-	flag.Parse()
 	flag.Parse()
 
 	configuration := &logging.ConfigurationStruct{}
@@ -44,7 +44,7 @@ func main() {
 
 	//Determine if configuration should be overridden from Consul
 	var consulMsg string
-	if useConsul == "y" {
+	if useConsul {
 		consulMsg = "Loading configuration from Consul..."
 		err := logging.ConnectToConsul(*configuration)
 		if err != nil {
