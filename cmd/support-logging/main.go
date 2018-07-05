f417e552c73d74a263caeb46232d526e7732c8e2
Brandon Forster <brandonforster@gmail.com>
2018-07-05 14:28:50 -0400

    Updated support-logging to include log of port it started on.
    Signed-off-by: Brandon Forster <brandonforster@gmail.com>

--- cmd/support-logging/main.go
+++ cmd/support-logging/main.go
@@ -22,6 +22,7 @@ import (
 	"github.com/edgexfoundry/edgex-go/internal/pkg/usage"
 	"github.com/edgexfoundry/edgex-go/support/logging"
 	"github.com/edgexfoundry/edgex-go/support/logging-client"
+	"strconv"
 )
 
 var loggingClient logger.LoggingClient
@@ -74,6 +75,7 @@ func main() {
 
 	// Time it took to start service
 	loggingClient.Info("Service started in: "+time.Since(start).String(), "")
+	loggingClient.Info("Listening on port: "+strconv.Itoa(configuration.Port), "")
 	logging.StartHTTPServer(errs)
 
 	c := <-errs
