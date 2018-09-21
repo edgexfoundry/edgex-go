3c0d7b093a714acf511641a116fc4bc23f8b622a
Ian Johnson <ian.johnson@canonical.com>
2018-09-21 15:26:50 -0500

    snap/Dockerfile: split into arch-specific dockerfiles
    This is necessary because dockerhub images don't have a concept of pulling/building an image per architecture, and so we have to manually specify what architecture of image we want when using a base image with FROM. 
    Also note that this means that cross-compilation using the dockerfiles is effectively impossible without using qemu-user-static, etc.
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- snap/build.sh
+++ snap/build.sh
@@ -4,8 +4,19 @@
 # snippet from https://stackoverflow.com/a/246128/10102404
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-# build the container image
-docker build -t edgex-snap-builder:latest -f ${SCRIPT_DIR}/Dockerfile.build .
+# build the container image - switch on which architecture we're on
+# note that for now the armhf and i386 builds fail, but if we ever support those targets
+# this will "just work"
+ARCH=$(arch)
+if [ $ARCH = "x86_64" ] ; then
+    docker build -t edgex-snap-builder:latest -f ${SCRIPT_DIR}/Dockerfile.amd64.build .
+elif [ $ARCH = "armhf" ] ; then
+    docker build -t edgex-snap-builder:latest -f ${SCRIPT_DIR}/Dockerfile.armhf.build .
+elif [ $ARCH = "aarch64" ] ; then
+    docker build -t edgex-snap-builder:latest -f ${SCRIPT_DIR}/Dockerfile.arm64.build .
+elif [ $ARCH = "i386" ] ; then
+    docker build -t edgex-snap-builder:latest -f ${SCRIPT_DIR}/Dockerfile.i386.build .
+fi
 
 # now run the build
 docker run --rm -v $(readlink -f ${SCRIPT_DIR}/..):/build edgex-snap-builder:latest
