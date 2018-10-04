faa3fea4a758f0ba3dabfa529b261c9cf2c46cc1
xmlviking <ecotter@gmail.com>
2018-10-04 15:07:25 -0500

    EdgeX-Go Support Scheduler initial check in
    Signed-off-by: xmlviking <ecotter@gmail.com>

--- bin/edgex-docker-launch.sh
+++ bin/edgex-docker-launch.sh
@@ -39,3 +39,5 @@ echo "Starting core-export-distro"
 docker-compose -f $COMPOSE_FILE up -d export-distro
 echo "Starting support-notifications"
 docker-compose -f $COMPOSE_FILE up -d notifications
+echo "Starting support-scheduler"
+docker-compose -f $COMPOSE_FILE up -d scheduler
