52d69eb96e27ebb0177c9e8f3cecbebcca1df157
Ian Johnson <ian.johnson@canonical.com>
2018-10-10 09:06:50 -0500

    snap/entrypoint.sh: fix login file path
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- snap/entrypoint.sh
+++ snap/entrypoint.sh
@@ -12,7 +12,7 @@ snapcraft
 # only on release jobs release the snap
 if [ "$IS_RELEASE_JOB" = "YES" ]; then
     # login using the provided login
-    snapcraft login --with /edgex-snap-store-login
+    snapcraft login --with /build/edgex-snap-store-login
     # push the snap up to the store and get the revision of the snap
     REVISION=$(snapcraft push edgexfoundry*.snap | awk '/Revision/ {print $2}')
     # now release it on the provided revision and snap channel
