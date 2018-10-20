e2078b992e889f20bef0b121b54290e2997ad540
Ian Johnson <ian.johnson@canonical.com>
2018-10-20 14:14:28 -0500

    snap/many: convert services into daemons; split up snap/local dir
    * Make all services independent daemons (except security-services) in snapcraft.yaml
    * Remove meta start-edgex and stop-edgex scripts, replace with independent wrappers for some daemons
    * Make all go service wrappers wait for consul to come up using curl before actually running
    * Make all go service wrappers using systemd-notify immediately to not get killed by systemd's timeout
    * Split up the snap/local directory into build-time scripts and runtime scripts; this helps with re-build times as the wrapper scripts, etc. all are runtime and if we need to change/rebuild those we shouldn't need to rebuild the go build helper, which then triggers a re-build of all the go parts.
    * The curl part now has as it's source dir the build-helpers directory, so that it doesn't default to ".", which then means it always needs to be rebuilt
    * Re-ordering the go parts to all depend on glide, which depends on go, which depends on the go-build-helper part. This also assists with faster iterative builds.
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

---
+++ snap/local/runtime-helpers/bin/wait-for-consul.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# unfortunately until snapd bug https://bugs.launchpad.net/snapd/+bug/1796125 
+# is fixed, on install services startup is not guaranteed in the right order
+# and as such, we need to do a little bit of hand holding for the go services,
+# which only have 10 seconds for consul to come alive, but on some systems
+# it can be more than 10 seconds until consul comes alive in which case
+# the services will fail to startup 
+# this gives consul 50 seconds to come up
+MAX_TRIES=10
+
+while [ "$MAX_TRIES" -gt 0 ] ; do
+    CONSUL_RUNNING=$(curl http://localhost:8500/v1/catalog/service/consul)
+
+    if [ $? -ne 0 ] ||
+       [ -z "$CONSUL_RUNNING" ] ||
+       [ "$CONSUL_RUNNING" = "[]" ] || [ "$CONSUL_RUNNING" = "" ]; then
+        echo "$1: consul not running; remaing tries: $MAX_TRIES"
+        sleep 5
+        MAX_TRIES=$(($MAX_TRIES - 1))
+    else
+	    break
+    fi
+done
