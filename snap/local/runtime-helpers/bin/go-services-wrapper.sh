78407993b4b12a6014cb64da4221ef43f1a43376
Ian Johnson <ian.johnson@canonical.com>
2018-11-06 16:11:13 -0600

    go-services-wrapper.sh: refactor to use other arguments
    This allows us to use the same wrapper script for config-seed as the other go services, and eventually sys-mgmt-agent which doesn't use the `--consul` argument here.
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- snap/local/runtime-helpers/bin/go-services-wrapper.sh
+++ snap/local/runtime-helpers/bin/go-services-wrapper.sh
@@ -8,4 +8,4 @@ $SNAP/bin/wait-for-consul.sh $1
 # if we get here, just assume that consul is up, otherwise this 
 # service will fail after 10 seconds
 cd $SNAP_DATA/config/$1
-$SNAP/bin/$1 --consul
+$SNAP/bin/$1 "${@:2}"
