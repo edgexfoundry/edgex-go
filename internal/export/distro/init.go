2d75935b12b1ee237a92dc76fcc154a80636ce24
Brandon Forster <brandonforster@gmail.com>
2019-01-11 00:09:16 -0600

    #970: Updated export-distro to use writable config
    Signed-off-by: Brandon Forster <brandonforster@gmail.com>

--- internal/export/distro/init.go
+++ internal/export/distro/init.go
@@ -154,8 +154,8 @@ func initializeConfiguration(useConsul bool, useProfile string) (*ConfigurationS
 func listenForConfigChanges() {
 	errCh := make(chan error)
 	dec := consulclient.NewConsulDecoder(Configuration.Registry)
-	dec.Target = &ConfigurationStruct{}
-	dec.Prefix = internal.ConfigRegistryStem + internal.ExportDistroServiceKey
+	dec.Target = &WritableInfo{}
+	dec.Prefix = internal.ConfigRegistryStem + internal.ExportDistroServiceKey + internal.WritableKey
 	dec.ErrCh = errCh
 	dec.UpdateCh = chConfig
 
@@ -169,11 +169,11 @@ func listenForConfigChanges() {
 			LoggingClient.Error(ex.Error())
 		case raw, ok := <-chConfig:
 			if ok {
-				actual, ok := raw.(*ConfigurationStruct)
+				actual, ok := raw.(*WritableInfo)
 				if !ok {
 					LoggingClient.Error("listenForConfigChanges() type check failed")
 				}
-				Configuration = actual //Mutex needed?
+				Configuration.Writable = *actual
 			} else {
 				return
 			}
