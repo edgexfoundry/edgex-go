89c9520f0c9afb3e29b3f0b5e9dcbbb771fd210c
Ian Johnson <person.uwsome@gmail.com>
2019-03-13 17:53:55 -0500

    mongo-worker-wrapper.sh: stop after 10 tries (#1150)
    This prevents us from running in an infinite loop.
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- snap/local/runtime-helpers/bin/mongo-worker-wrapper.sh
+++ snap/local/runtime-helpers/bin/mongo-worker-wrapper.sh
@@ -1,7 +1,14 @@
-#!/bin/bash -e
+#!/bin/bash
 
-
-while true; do
-  mongo $SNAP/mongo/init_mongo.js && break
-  sleep 5
+# try to initialize mongo, giving up after a reasonable number of tries
+MAX_TRIES=10
+num_tries=0
+until mongo $SNAP/mongo/init_mongo.js; do
+    sleep 5
+    # increment number of tries
+    num_tries=$((num_tries+1))
+    if (( num_tries >= MAX_TRIES )); then
+        echo "unable to init mongod from mongo-worker after $MAX_TRIES attempts"
+        exit 1
+    fi
 done
