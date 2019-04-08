196e766aa6af5194b0b86e5d201d9039e8cdc9a4
Ian Johnson <ian.johnson@canonical.com>
2019-04-07 21:36:26 -0500

    kong-daemon.sh: don't timeout manually in script, rely on systemd
    Instead of manually implementing a timeout here, we can just run in an
    infinite loop, relying on the fact that systemd will kill us if kong
    takes too long to startup.
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- snap/local/runtime-helpers/bin/kong-daemon.sh
+++ snap/local/runtime-helpers/bin/kong-daemon.sh
@@ -3,20 +3,11 @@
 # the kong wrapper script from $SNAP
 export KONG_SNAP="${SNAP}/bin/kong-wrapper.sh"
 
-export num_tries=0
-export MAX_KONG_UP_TRIES=10
-
 # run kong migrations up to bootstrap the cassandra database
 # note that sometimes cassandra can be in a "starting up" state, etc.
 # and in this case we should just loop and keep trying
 until $KONG_SNAP migrations up --yes --conf $KONG_CONF; do
-    sleep 10
-    # increment number of tries
-    num_tries=$((num_tries+1))
-    if (( num_tries > MAX_KONG_UP_TRIES )); then
-        echo "max tries attempting to bring up kong"
-        exit 1
-    fi
+    sleep 5
 done
 
 # now start kong normally
