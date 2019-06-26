86f80c4fa76c5de8b74f6e0ba507db4caec5dd66
Anthony M. Bonafide <AnthonyMBonafide@gmail.com>
2019-06-26 10:43:33 -0400

    Refactor addressable endpoints to use operators
    Fix #1478
    
    Refactor addressable REST endpoints 'restGetAddressableByAddress' and
    'restGetAddressableByPublisher' to use the new operator pattern to allow
    for easier testing as part of the effort to increase test code coverage
    
    Add new operators to provide the necessary functionality needed by the
    REST endpoints
    
    Add tests to cover the various Execute methods available by the newly
    added addressable operators
    
    Add tests to the router functions which have been updated to use the new
    operators
    
    Signed-off-by: Anthony M. Bonafide <AnthonyMBonafide@gmail.com>

--- internal/core/metadata/operators/addressable/db.go
+++ internal/core/metadata/operators/addressable/db.go
@@ -1,9 +1,10 @@
-// addressable contains functionality for obtaining Addressable data
+// addressable contains functionality for obtaining Addressable data.
 package addressable
 
 import contract "github.com/edgexfoundry/go-mod-core-contracts/models"
 
-// AddressLoader provides functionality for obtaining Addressables given an address
+// AddressLoader provides functionality for obtaining Addressables.
 type AddressLoader interface {
 	GetAddressablesByAddress(address string) ([]contract.Addressable, error)
+	GetAddressablesByPublisher(p string) ([]contract.Addressable, error)
 }
--- internal/core/metadata/operators/addressable/get.go
+++ internal/core/metadata/operators/addressable/get.go
@@ -1,27 +1,51 @@
-// addressable contains functionality for obtaining Addressable data
+// addressable contains functionality for obtaining Addressable data.
 package addressable
 
 import contract "github.com/edgexfoundry/go-mod-core-contracts/models"
 
-// AddressablesRetreiver provide functionality for retrieving addresses from an underlying data-store
-type AddressablesRetreiver interface {
+// AddressExecutor provides functionality for retrieving addresses from an underlying data-store.
+type AddressExecutor interface {
 	Execute() ([]contract.Addressable, error)
 }
 
-// addressLoader loads address by way of the operator pattern
-type addressLoader struct {
+// addressLoadByAddress loads address by way of the operator pattern.
+type addressLoadByAddress struct {
 	database AddressLoader
 	address  string
 }
 
-// Execute retrieves the addressables from the underlying data-store
-func (n addressLoader) Execute() ([]contract.Addressable, error) {
+// Execute retrieves the addressables from the underlying data-store.
+func (n addressLoadByAddress) Execute() ([]contract.Addressable, error) {
 	return n.database.GetAddressablesByAddress(n.address)
 }
 
-// NewAddressLoader creates an AddressablesRetreiver
-func NewAddressLoader(db AddressLoader, address string) AddressablesRetreiver {
-	return addressLoader{
+// NewAddressExecutor creates an AddressExecutor.
+func NewAddressExecutor(db AddressLoader, address string) AddressExecutor {
+	return addressLoadByAddress{
 		database: db,
 		address:  address}
+}
+
+// PublisherExecutor provides functionality for retrieving addresses from an underlying data-store.
+type PublisherExecutor interface {
+	Execute() ([]contract.Addressable, error)
+}
+
+// PublisherExecutor loads address by way of the operator pattern.
+type addressLoadByPublisher struct {
+	database  AddressLoader
+	publisher string
+}
+
+// Execute retrieves the addressables from the underlying data-store.
+func (p addressLoadByPublisher) Execute() ([]contract.Addressable, error) {
+	return p.database.GetAddressablesByPublisher(p.publisher)
+}
+
+// NewAddressExecutor creates a PublisherExecutor.
+func NewPublisherExecutor(db AddressLoader, publisher string) PublisherExecutor {
+	return addressLoadByPublisher{
+		database:  db,
+		publisher: publisher,
+	}
 }
