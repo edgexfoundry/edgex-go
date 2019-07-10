372781e7cefe8f781ab3fc5dab71c43cae451919
difince <dianaa@vmware.com>
2019-07-10 17:40:29 +0300

    Add First Steps in Implementing Core-Command Persistent Layer
    - Added direct connection from core-command service to "metadata" database
    (should be change in future, core-command is supposed to have its own database)
    - Added DBClient interface
    - Add new GetCommandByNameAndDeviceId function in the BDClient
    - Rewrite all functions that called metadata endpoints for quering commands to use
    newly edded DBClient interface
    - rename GetCommandByName function to GetCommandsByName (plural)
    
    Fix: https://github.com/edgexfoundry/edgex-go/issues/1274
    
    Signed-off-by: difince <dianaa@vmware.com>

---
+++ internal/core/command/interfaces/db.go
@@ -0,0 +1,13 @@
+package interfaces
+
+import (
+	contract "github.com/edgexfoundry/go-mod-core-contracts/models"
+)
+
+type DBClient interface {
+	GetAllCommands() ([]contract.Command, error)
+	GetCommandById(id string) (contract.Command, error)
+	GetCommandsByName(id string) ([]contract.Command, error)
+	GetCommandsByDeviceId(id string) ([]contract.Command, error)
+	GetCommandByNameAndDeviceId(cname string, did string) (contract.Command, error)
+}
