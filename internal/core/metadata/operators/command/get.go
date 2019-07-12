b1326a6be4c26c03c308181c45c4c51c58e32b1f
Diana Atanasova <dianaa@vmware.com>
2019-07-12 21:03:44 +0300

    Unit Tests -- Core-Metadata: Get All Commands (#1529)
    Fix #1527
    
    Signed-off-by: difince <dianaa@vmware.com>

--- internal/core/metadata/operators/command/get.go
+++ internal/core/metadata/operators/command/get.go
@@ -1,6 +1,8 @@
 package command
 
 import (
+	"github.com/edgexfoundry/edgex-go/internal/core/metadata/errors"
+	"github.com/edgexfoundry/edgex-go/internal/pkg/config"
 	contract "github.com/edgexfoundry/go-mod-core-contracts/models"
 )
 
@@ -20,4 +22,31 @@ func NewDeviceIdExecutor(db CommandLoader, deviceId string) DeviceIdExecutor {
 
 func (op commandLoadByDeviceId) Execute() (commands []contract.Command, err error) {
 	return op.database.GetCommandsByDeviceId(op.deviceId)
+}
+
+//Get All commands
+type CommandAllExecutor interface {
+	Execute() ([]contract.Command, error)
+}
+
+type commandLoadAll struct {
+	config   config.ServiceInfo
+	database CommandLoader
+}
+
+func NewCommandLoadAll(cfg config.ServiceInfo, db CommandLoader) CommandAllExecutor {
+	return commandLoadAll{config: cfg, database: db}
+}
+
+func (op commandLoadAll) Execute() (cmds []contract.Command, err error) {
+	cmds, err = op.database.GetAllCommands()
+	if err != nil {
+		return
+	}
+
+	if len(cmds) > op.config.MaxResultCount {
+		err = errors.NewErrLimitExceeded(op.config.MaxResultCount)
+		return cmds, err
+	}
+	return
 }
