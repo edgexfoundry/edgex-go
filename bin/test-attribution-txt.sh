82597d120bf884c3eefbc7396c3d156c7447d534
Ian Johnson <ian.johnson@canonical.com>
2019-08-15 05:42:19 -0500

    bin/test-attribution-txt.sh: add test for attribution files
    Check that all of the modules listed by go mod in the vendor directory when
    running `go mod vendor` are attributed in the Attribution.txt files in the cmd
    directories.
    
    Also ensure that there is an Attribution.txt file in each of the
    cmd directories.
    
    Add this check to make test to fail CI verify jobs if the Attribution.txt files
    are missing attributions or executables in cmd are missing an Attribution.txt.
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

---
+++ bin/test-attribution-txt.sh
@@ -0,0 +1,60 @@
+#!/bin/bash -e
+
+# get the directory of this script
+# snippet from https://stackoverflow.com/a/246128/10102404
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+GIT_ROOT=$(dirname "$SCRIPT_DIR")
+
+EXIT_CODE=0
+
+cd "$GIT_ROOT"
+
+if [ -d vendor.bk ]; then
+    echo "vendor.bk exits - remove before continuing"
+    exit 1
+fi
+
+trap cleanup 1 2 3 6
+
+cleanup()
+{
+    cd "$GIT_ROOT"
+    # restore the vendor dir
+    rm -r vendor
+    if [ -d vendor.bk ]; then
+        mv vendor.bk vendor
+    fi
+    exit $EXIT_CODE
+}
+
+# if the vendor directory exists, back it up so we can build a fresh one
+if [ -d vendor ]; then
+    mv vendor vendor.bk
+fi
+
+# create a vendor dir with the mod dependencies
+GO111MODULE=on go mod vendor
+
+# turn on nullglobbing so if there is nothing in cmd dir then we don't do
+# anything in this loop
+shopt -s nullglob
+
+for cmd in cmd/* ; do
+    cd "$cmd"
+        if [ ! -f Attribution.txt ]; then
+            echo "An Attribution.txt file for $cmd is missing, please add"
+            EXIT_CODE=1
+        else
+            # loop over every library in the modules.txt file in vendor
+            while IFS= read -r lib; do
+                if ! grep -q "$lib" Attribution.txt; then 
+                    echo "An attribution for $lib is missing from in $cmd Attribution.txt, please add"
+                    # need to do this in a bash subshell, see SC2031
+                    (( EXIT_CODE=1 ))
+                fi
+            done < <(grep '#' < "$GIT_ROOT/vendor/modules.txt" | awk '{print $2}')
+        fi
+    cd "$GIT_ROOT"
+done
+
+cleanup 
