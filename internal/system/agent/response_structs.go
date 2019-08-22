94a1ae234ac3bfd0dc19b466d24c200457f1610b
Michael Estrin <m.estrin@dell.com>
2019-08-22 13:05:37 -0500

    SMA to get Metrics via Executor
    Refactored executor to minimal example implementation.  Shells out to docker to obtain metrics and execute requested operations.  Executor always returns JSON result.  Implemented full use-case test coverage for executor.  Removed service check/limitation.
    
    Refactored agent to align with executor changes.  Condensed code and made it more consistent.
    
    Implemented minimal standard contract for metrics (i.e. cpuUsagePercent, memoryUsed) to be supplied by all metric gathering implementations.  Also return executor type and raw result from metric gathering implementation if upstream consumer wants to access implementation-specific data.
    
    https://github.com/edgexfoundry/edgex-go/issues/1486
    Signed-off-by: Michael Estrin <m.estrin@dell.com>

--- internal/system/agent/response_structs.go
+++
@@ -1,9 +0,0 @@
-package agent
-
-type ConfigRespMap struct {
-	Configuration map[string]interface{}
-}
-
-type MetricsRespMap struct {
-	Metrics map[string]interface{}
-}
---
+++ internal/system/executor/metrics.go
@@ -0,0 +1,89 @@
+/*******************************************************************************
+ * Copyright 2019 Dell Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *******************************************************************************/
+
+package executor
+
+import (
+	"fmt"
+	"strings"
+)
+
+const separator = ";"
+
+// metricsExecutorCommands returns the Docker command to be executed to gather metrics from the Docker CLI.
+func metricsExecutorCommands(serviceName string) []string {
+	return []string{
+		"stats",
+		serviceName,
+		"--no-stream",
+		"--format",
+		"{{ .CPUPerc }}" + separator +
+			"{{ .MemUsage }}" + separator +
+			"{\"cpu_perc\":\"{{ .CPUPerc }}\",\"mem_usage\":\"{{ .MemUsage }}\",\"mem_perc\":\"{{ .MemPerc }}\",\"net_io\":\"{{ .NetIO }}\",\"block_io\":\"{{ .BlockIO }}\",\"pids\":\"{{ .PIDs }}\"}",
+	}
+}
+
+// dockerCpuToFloat converts the CPU value returned from the Docker CLI to a float64 value.
+func dockerCpuToFloat(value string) float64 {
+	var cpu float64
+	n, err := fmt.Sscanf(value, "%f", &cpu)
+	if err != nil || n != 1 {
+		return -1.0
+	}
+	return cpu
+}
+
+// dockerMemoryToInt converts the memory used value returned from the Docker CLI to an int64 value.
+func dockerMemoryToInt(value string) int64 {
+	const (
+		kb = 1024
+		mb = kb * 1024
+		gb = mb * 1024
+	)
+	var memory float64
+	var scale string
+	n, err := fmt.Sscanf(value, "%f%s", &memory, &scale)
+	if err != nil || n != 2 {
+		return -1
+	}
+	switch scale {
+	case "KiB":
+		memory *= kb
+	case "MiB":
+		memory *= mb
+	case "GiB":
+		memory *= gb
+	}
+	return int64(memory + 0.5)
+}
+
+// resultToFields converts and returns the values returned by the Docker CLI into normalized cpuUsedPercent and
+// memoryUsed fields provided by every executor (along with the raw Docker CLI result).
+func resultToFields(result string) (cpuUsedPercent float64, memoryUsed int64, raw []byte) {
+	resultFields := strings.Split(strings.TrimRight(result, "\n"), separator)
+	cpuUsedPercent = dockerCpuToFloat(resultFields[0])
+	memoryUsed = dockerMemoryToInt(strings.Split(resultFields[1], " ")[0])
+	raw = []byte(resultFields[2])
+	return
+}
+
+// gatherMetrics delegates metrics gathering to the executor and converts the result to a MetricsSuccessResult.
+func gatherMetrics(serviceName string, executor CommandExecutor) Result {
+	result, err := executor(metricsExecutorCommands(serviceName)...)
+	if err != nil {
+		return Failure(serviceName, Metrics, executorType, err.Error())
+	}
+	cpuUsedPercent, memoryUsed, raw := resultToFields(string(result))
+	return MetricsSuccess(serviceName, executorType, cpuUsedPercent, memoryUsed, raw)
+}
