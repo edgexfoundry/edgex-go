562ef9ad15f6da8b07eac1c1c5657800b9c340a8
tingyuz <37156729+tingyuz@users.noreply.github.com>
2019-09-13 16:36:56 -0400

    Add security-secretstore-setup into edgex-go
    Fix #1673 
    
    * rename configurations.toml to configuration.toml
    * remove version number in the header of the files; add dummy script for secretstore initialization
    * reformat the main.go file
    * add Attribution.txt file
    * remove lines that will be added in the future for other features
    * remove script in the project
    * set default value of insecureSkipVerify as false
    
    Signed-off-by: Tingyu Zeng <tingyu.zeng@rsa.com>

---
+++ cmd/security-secretstore-setup/main.go
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright 2019 Dell Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * @author: Alain Pulluelo, ForgeRock AS
+ * @author: Tingyu Zeng, Dell
+ *
+ *******************************************************************************/
+
+package main
+
+import (
+	"flag"
+	"os"
+
+	"github.com/edgexfoundry/edgex-go/internal/pkg/usage"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		usage.HelpCallbackSecuritySecretStore()
+	}
+
+	var initNeeded bool
+	var insecureSkipVerify bool
+	var configFileLocation string
+	var waitInterval int
+	var useProfile string
+	var useRegistry bool
+
+	flag.BoolVar(&initNeeded, "init", false, "run init procedure for security service.")
+	flag.BoolVar(&insecureSkipVerify, "insecureSkipVerify", false, "skip server side SSL verification, mainly for self-signed cert")
+	flag.StringVar(&configFileLocation, "configfile", "res/configuration.toml", "configuration file")
+	flag.IntVar(&waitInterval, "wait", 30, "time to wait between checking Vault status in seconds.")
+	flag.BoolVar(&useRegistry, "registry", false, "Indicates the service should use registry service.")
+	flag.BoolVar(&useRegistry, "r", false, "Indicates the service should use registry service.")
+	flag.StringVar(&useProfile, "profile", "", "Specify a profile other than default.")
+	flag.StringVar(&useProfile, "p", "", "Specify a profile other than default.")
+
+	flag.Usage = usage.HelpCallbackSecuritySecretStore
+	flag.Parse()
+
+	//step 1: boot up secretstore general steps same as other EdgeX microservice
+
+	//step 2: initialize the communications
+
+	//step 3: initialize and unseal Vault
+
+	//Step 4:
+	//TODO: create vault access token for different roles
+
+	//step 5 :
+	//TODO: implment credential creation
+
+	//step 6:  Push cert key pair for KONG into Vault
+
+}
--- internal/pkg/usage/usage.go
+++ internal/pkg/usage/usage.go
@@ -67,6 +67,19 @@ Common Options:
 	-h, --help                      Show this message
 `
 
+var securitySecretStoreSetupUsageStr = `
+Usage: %s [options]
+Server Options:
+	-p, --profile <name>            Indicate configuration profile other than default
+	-r, --registry                  Indicates service should use Registry
+	--insecureSkipVerify=true/false			Indicates if skipping the server side SSL cert verifcation, similar to -k of curl
+	--init=true/false				Indicates if security service should be initialized
+	--configfile=<file.toml>			Use a different config file (default: res/configuration.toml)
+	--wait=<time in seconds>		Indicates how long the program will pause between the vault initialization until it succeeds
+Common Options:
+	-h, --help					Show this message
+`
+
 // usage will print out the flag options for the server.
 func HelpCallback() {
 	fmt.Printf(usageStr, os.Args[0])
@@ -84,5 +97,11 @@ func HelpCallbackSecuritySetup() {
 
 func HelpCallbackSecurityProxy() {
 	fmt.Printf(securityProxySetupUsageStr, os.Args[0])
+	os.Exit(0)
+}
+
+func HelpCallbackSecuritySecretStore() {
+	msg := fmt.Sprintf(securitySecretStoreSetupUsageStr, os.Args[0])
+	fmt.Printf("%s\n", msg)
 	os.Exit(0)
 }
