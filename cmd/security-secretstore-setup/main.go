ea1f15c5e52ec41aeee6b49e69e44486ee9ba6fa
Daniel Harms <jdharms@gmail.com>
2019-09-19 10:44:20 -0400

    load kong cert pair from filesystem to vault (#1777)
    Fix #1675
    
    * load kong cert pair from filesystem to vault
    * Address code review feedback
    
    Signed-off-by: Daniel Harms <jdharms@gmail.com>

--- cmd/security-secretstore-setup/main.go
+++ cmd/security-secretstore-setup/main.go
@@ -13,6 +13,7 @@
  *
  * @author: Alain Pulluelo, ForgeRock AS
  * @author: Tingyu Zeng, Dell
+ * @author: Daniel Harms, Dell
  *
  *******************************************************************************/
 
@@ -21,8 +22,14 @@ package main
 import (
 	"flag"
 	"os"
+	"path/filepath"
 
+	"github.com/edgexfoundry/edgex-go/internal"
+	"github.com/edgexfoundry/edgex-go/internal/pkg/startup"
 	"github.com/edgexfoundry/edgex-go/internal/pkg/usage"
+	"github.com/edgexfoundry/edgex-go/internal/security/secretstore"
+	"github.com/edgexfoundry/go-mod-core-contracts/clients/logger"
+	"github.com/edgexfoundry/go-mod-core-contracts/models"
 )
 
 func main() {
@@ -49,9 +56,13 @@ func main() {
 	flag.Usage = usage.HelpCallbackSecuritySecretStore
 	flag.Parse()
 
+	params := startup.BootParams{UseRegistry: useRegistry, UseProfile: useProfile, BootTimeout: internal.BootTimeoutDefault}
+	startup.Bootstrap(params, secretstore.Retry, logBeforeInit)
+
 	//step 1: boot up secretstore general steps same as other EdgeX microservice
 
 	//step 2: initialize the communications
+	req := secretstore.NewRequester(insecureSkipVerify)
 
 	//step 3: initialize and unseal Vault
 
@@ -59,8 +70,42 @@ func main() {
 	//TODO: create vault access token for different roles
 
 	//step 5 :
-	//TODO: implment credential creation
+	//TODO: implement credential creation
 
-	//step 6:  Push cert key pair for KONG into Vault
+	absTokenPath := filepath.Join(secretstore.Configuration.SecretService.TokenFolderPath, secretstore.Configuration.SecretService.TokenFile)
+	cert := secretstore.NewCerts(req, secretstore.Configuration.SecretService.CertPath, absTokenPath)
+	existing, err := cert.AlreadyinStore()
+	if err != nil {
+		secretstore.LoggingClient.Error(err.Error())
+		os.Exit(1)
+	}
 
+	if existing == true {
+		secretstore.LoggingClient.Info("proxy certificate pair are in the secret store already, skip uploading")
+		os.Exit(0)
+	}
+
+	secretstore.LoggingClient.Info("proxy certificate pair are not in the secret store yet, uploading them")
+	cp, err := cert.ReadFrom(secretstore.Configuration.SecretService.CertFilePath, secretstore.Configuration.SecretService.KeyFilePath)
+	if err != nil {
+		secretstore.LoggingClient.Error("failed to get certificate pair from volume")
+		os.Exit(1)
+	}
+
+	secretstore.LoggingClient.Info("proxy certificate pair are loaded from volume successfully, will upload to secret store")
+
+	err = cert.UploadToStore(cp)
+	if err != nil {
+		secretstore.LoggingClient.Error("failed to upload the proxy cert pair into the secret store")
+		secretstore.LoggingClient.Error(err.Error())
+		os.Exit(1)
+	}
+
+	secretstore.LoggingClient.Info("proxy certificate pair are uploaded to secret store successfully, Vault init done successfully")
+	os.Exit(0)
+}
+
+func logBeforeInit(err error) {
+	l := logger.NewClient("security-secretstore-setup", false, "", models.InfoLog)
+	l.Error(err.Error())
 }
---
+++ internal/security/secretstore/certs_test.go
@@ -0,0 +1,96 @@
+/*******************************************************************************
+ * Copyright 2019 Dell Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * @author: Tingyu Zeng, Dell
+ * @author: Daniel Harms, Dell
+ *******************************************************************************/
+
+package secretstore
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strconv"
+	"testing"
+
+	"github.com/edgexfoundry/go-mod-core-contracts/clients/logger"
+)
+
+func TestGetAccessToken(t *testing.T) {
+	path := "testdata/test-resp-init.json"
+	s, err := GetAccessToken(path)
+	if err != nil {
+		t.Errorf("failed to parse token file")
+		t.Errorf(err.Error())
+	}
+	if s != "test-root-token" {
+		t.Errorf("incorrect token")
+		t.Errorf(s)
+	}
+}
+
+func TestRetrieve(t *testing.T) {
+	LoggingClient = logger.MockLogger{}
+
+	certPath := "testCertPath"
+	token := "token"
+	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"data": {"cert": "test-certificate", "key": "test-private-key"}}`))
+		if r.Method != "GET" {
+			t.Errorf("expected GET request, got %s instead", r.Method)
+		}
+
+		if r.URL.EscapedPath() != fmt.Sprintf("/%s", certPath) {
+			t.Errorf("expected request to /%s, got %s instead", certPath, r.URL.EscapedPath())
+		}
+
+		if r.Header.Get(VaultToken) != token {
+			t.Errorf("expected request header for %s is %s, got %s instead", VaultToken, token, r.Header.Get(VaultToken))
+		}
+	}))
+	defer ts.Close()
+
+	parsed, err := url.Parse(ts.URL)
+	if err != nil {
+		t.Errorf("unable to parse test server URL %s", ts.URL)
+		return
+	}
+	port, err := strconv.Atoi(parsed.Port())
+	if err != nil {
+		t.Errorf("parsed port number cannot be converted to int %s", parsed.Port())
+		return
+	}
+
+	oldConfig := Configuration
+	defer func() { Configuration = oldConfig }()
+
+	Configuration = &ConfigurationStruct{}
+	Configuration.SecretService = SecretServiceInfo{
+		Server: parsed.Hostname(),
+		Port:   port,
+		Scheme: "https",
+	}
+
+	cs := NewCerts(NewRequester(true), certPath, "")
+	cp, err := cs.retrieve(token)
+	if err != nil {
+		t.Errorf("failed to retrieve cert pair")
+		t.Errorf(err.Error())
+	}
+	if cp.Cert != "test-certificate" || cp.Key != "test-private-key" {
+		t.Errorf("failed to parse certificate key pair")
+	}
+}
