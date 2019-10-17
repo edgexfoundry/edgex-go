4c843c0afd1d7b616fe61dbf8c48e4c8c2b017e8
Ian Johnson <ian.johnson@canonical.com>
2019-10-17 13:58:57 -0500

    snap/many: use postgres in the snap for kong's DB
    * Remove Cassandra and replace it with PostgreSQL from the Ubuntu binoic
      archives. PostgreSQL is faster, more resource efficient and smaller in size by
      about 65M.
    * Add snapcraft-preload to the snap to be used with postgres and related apps,
      to prevent usage of /dev/shm/... for the lock files that postgres creates.
      Instead these lockfiles are redirected to /dev/shm/snap.$SNAP_INSTANCE_NAME.
      Note that snapcraft-preload must be built before Kong because of some build
      weirdness, see the code comments for a full explanation.
      which snapd security policy allows.
    * Because PostgreSQL doesn't allow running as root, declare system-usernames as
      using snap_daemon shared and drop to this user for running various postgres
      apps and the postgres daemon. This also requires bumping the minimum snapd
      version to 2.42, where system-usernames was introduced.
    * Provide psql, psql-any (which is like psql but doesn't do any privilege
      dropping), createuser, and createdb apps for administering the postgres
      database, mainly for development purposes
    * During the install hook, setup and configure postgres user and database for
      Kong during initial install so that Kong can just start running without user
      intervention or action.
    * Add gosu part to make privilege dropping simple without the complexities of
      sudo (which is denied inside snaps) or su (which handles TTY's weirdly)
    * Refactor the wrapper scripts and such to be simpler and use command-chain more
      heavily.
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- snap/local/runtime-helpers/bin/kong-daemon.sh
+++ snap/local/runtime-helpers/bin/kong-daemon.sh
@@ -10,17 +10,14 @@ if [ "$SNAP_ARCH" = "arm64" ]; then
   exit 0
 fi
 
-# the kong wrapper script from $SNAP
-export KONG_SNAP="$SNAP/bin/kong-wrapper.sh"
-
 # run kong migrations up to bootstrap the cassandra database
 # note that sometimes cassandra can be in a "starting up" state, etc.
 # and in this case we should just loop and keep trying
 # we don't implement a timeout here because systemd will kill us if we 
 # don't succeed in 15 minutes (or whatever the configured stop-timeout is)
-until $KONG_SNAP migrations bootstrap --conf "$KONG_CONF"; do
+until kong migrations bootstrap --conf "$KONG_CONF"; do
     sleep 5
 done
 
 # now start kong normally
-$KONG_SNAP start --conf "$KONG_CONF"
+kong start --conf "$KONG_CONF" --v
---
+++ snap/local/runtime-helpers/bin/perl5lib-launch.sh
@@ -0,0 +1,35 @@
+#!/bin/sh -e
+
+# figure out the snap architecture lib name
+case $SNAP_ARCH in
+    amd64)
+        ARCH_LIB_NAME="x86_64-linux-gnu"
+        ;;
+    armhf)
+        ARCH_LIB_NAME="arm-linux-gnueabihf"
+        ;;
+    arm64)
+        ARCH_LIB_NAME="aarch64-linux-gnu"
+        ;;
+    i386)
+        ARCH_LIB_NAME="i386-linux-gnu"
+        ;;
+    *)
+        # unsupported or unknown architecture
+        exit 1
+        ;;
+esac
+
+export ARCH_LIB_NAME
+
+# get the perl version
+PERL_VERSION=$(perl -version | grep -Po '\(v\K([^\)]*)')
+
+# perl lib paths are needed for some rocks that kong loads through luarocks dependencies
+PERL5LIB="$PERL5LIB:$SNAP/usr/lib/$ARCH_LIB_NAME/perl/$PERL_VERSION"
+PERL5LIB="$PERL5LIB:$SNAP/usr/share/perl/$PERL_VERSION"
+PERL5LIB="$PERL5LIB:$SNAP/usr/share/perl5"
+
+export PERL5LIB
+
+exec "$@"
