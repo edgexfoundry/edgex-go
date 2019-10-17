995fb74d1860133791b29ae86aacd33aa1d813ba
Ian Johnson <ian.johnson@canonical.com>
2019-10-17 13:58:57 -0500

    snap/many: use postgres in the snap for kong's DB
    * Remove Cassandra and replace it with PostgreSQL from the Ubuntu binoic
      archives. PostgreSQL is faster, more resource efficient and smaller in size by
      about 65M.
    * Add snapcraft-preload to the snap to be used with postgres and related apps,
      to prevent usage of /dev/shm/... for the lock files that postgres creates.
      Instead these lockfiles are redirected to /dev/shm/snap.$SNAP_INSTANCE_NAME.
      Note that snapcraft-preload must be built before Kong because of some build
      weirdness, see the code comments for a full explanation.
      which snapd security policy allows.
    * Because PostgreSQL doesn't allow running as root, declare system-usernames as
      using snap_daemon shared and drop to this user for running various postgres
      apps and the postgres daemon. This also requires bumping the minimum snapd
      version to 2.42, where system-usernames was introduced.
    * Provide psql, psql-any (which is like psql but doesn't do any privilege
      dropping), createuser, and createdb apps for administering the postgres
      database, mainly for development purposes
    * During the install hook, setup and configure postgres user and database for
      Kong during initial install so that Kong can just start running without user
      intervention or action.
    * Add gosu part to make privilege dropping simple without the complexities of
      sudo (which is denied inside snaps) or su (which handles TTY's weirdly)
    * Refactor the wrapper scripts and such to be simpler and use command-chain more
      heavily.
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

---
+++ snap/local/runtime-helpers/bin/gosu-snap_daemon.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+
+gosu snap_daemon "$@"
---
+++ snap/local/runtime-helpers/bin/kong-launch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+
+# need to add luajit/lib folder so that luajit will load properly
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SNAP/luajit/lib"
+
+# lua paths so that luarocks can work
+export LUA_VERSION=5.1
+export LUA_PATH="$SNAP/lualib/?.lua;$SNAP/lualib/?/init.lua;$SNAP/usr/share/lua/$LUA_VERSION/?.lua;$SNAP/usr/share/lua/$LUA_VERSION/?/init.lua;$SNAP/lib/lua/$LUA_VERSION/?.lua;$SNAP/lib/lua/$LUA_VERSION/?/init.lua;$SNAP/share/lua/$LUA_VERSION/?.lua;$SNAP/share/lua/$LUA_VERSION/?/init.lua;;"
+export LUA_CPATH="$SNAP/lualib/?.so;$SNAP/lib/lua/$LUA_VERSION/?.so;$SNAP/lib/$ARCH_LIB_NAME/lua/$LUA_VERSION/?.so;;"
+
+# also make sure our logs dir exists because for some reason nginx can't create
+# it's own logs dir
+mkdir -p "$KONG_LOGS_DIR"
+
+exec "$@"
