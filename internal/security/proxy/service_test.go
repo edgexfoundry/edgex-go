ef7f4719c799836406ba16e0612437f4f7dec3c1
Tingyu Zeng <Tingyu.Zeng@rsa.com>
2019-11-15 16:20:18 -0500

    Fix issue-2089 for master branch
    Instead of returning two variables (error and bool) , postCert() returns one custom error and Init() checks the error type for further action.
    
    Signed-off-by: Tingyu Zeng <Tingyu.Zeng@rsa.com>

--- internal/security/proxy/service_test.go
+++ internal/security/proxy/service_test.go
@@ -37,6 +37,54 @@ func (m mockCertificateLoader) Load() (*CertPair, error) {
 	return &CertPair{"test-certificate", "test-private-key"}, nil
 }
 
+func TestPostCertExists(t *testing.T) {
+	fileName := "./testdata/configuration.toml"
+	contents, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		t.Errorf("could not load configuration file (%s): %s", fileName, err.Error())
+		return
+	}
+
+	configuration := &config.ConfigurationStruct{}
+	err = toml.Unmarshal(contents, configuration)
+	if err != nil {
+		t.Errorf("unable to parse configuration file (%s): %s", fileName, err.Error())
+		return
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`"edgex-kong already associated with existing certificate"`))
+	}))
+	defer ts.Close()
+
+	host, port, err := parseHostAndPort(ts, t)
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
+	configuration.KongURL = config.KongUrlInfo{
+		Server:    host,
+		AdminPort: port,
+	}
+
+	mock := mockCertificateLoader{}
+	mockLogger := logger.MockLogger{}
+	service := NewService(NewRequestor(true, 10, "", mockLogger), mockLogger, configuration)
+	e := service.postCert(mock)
+	if e == nil {
+		t.Errorf("failed on testing existing certificate on proxy - failed to catch bad request")
+	}
+	if e.reason != CertExisting {
+		t.Errorf("failed on testing existing certificate on proxy - failed to catch message of existing cert")
+	}
+}
+
 func TestInit(t *testing.T) {
 	fileName := "./testdata/configuration.toml"
 	contents, err := ioutil.ReadFile(fileName)
