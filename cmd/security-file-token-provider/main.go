eb9431f8b5943e74b364c296529e5d6ab2985497
Bryon Nevis <bryon.nevis@intel.com>
2019-11-22 08:40:50 -0800

    Implement cmd/security-file-token-provider scaffolding
    This is the main program bootstrap skeleton for
    cmd/security-file-token-provider as documented in
    edgex-docs/security/token-file-provider.1.rst
    
    Signed-off-by: Bryon Nevis <bryon.nevis@intel.com>

---
+++ cmd/security-file-token-provider/main.go
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright 2019 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ *******************************************************************************/
+
+package main
+
+import (
+	"flag"
+
+	"github.com/edgexfoundry/edgex-go/internal"
+	"github.com/edgexfoundry/edgex-go/internal/pkg/bootstrap"
+	bootstrapContainer "github.com/edgexfoundry/edgex-go/internal/pkg/bootstrap/container"
+	"github.com/edgexfoundry/edgex-go/internal/pkg/bootstrap/interfaces"
+	"github.com/edgexfoundry/edgex-go/internal/pkg/bootstrap/startup"
+	"github.com/edgexfoundry/edgex-go/internal/pkg/di"
+	"github.com/edgexfoundry/edgex-go/internal/pkg/usage"
+	"github.com/edgexfoundry/edgex-go/internal/security/fileprovider"
+	"github.com/edgexfoundry/edgex-go/internal/security/fileprovider/config"
+	"github.com/edgexfoundry/edgex-go/internal/security/fileprovider/container"
+	"github.com/edgexfoundry/go-mod-core-contracts/clients"
+)
+
+func main() {
+	startupTimer := startup.NewStartUpTimer(internal.BootRetrySecondsDefault, internal.BootTimeoutSecondsDefault)
+
+	var configDir, profileDir string
+	var useRegistry bool
+
+	flag.BoolVar(&useRegistry, "registry", false, "Indicates the service should use registry service.")
+	flag.BoolVar(&useRegistry, "r", false, "Indicates the service should use registry service.")
+	flag.StringVar(&profileDir, "profile", "", "Specify a profile other than default.")
+	flag.StringVar(&profileDir, "p", "", "Specify a profile other than default.")
+	flag.StringVar(&configDir, "confdir", "", "Specify local configuration directory")
+
+	flag.Usage = usage.HelpCallbackSecurityFileTokenProvider
+	flag.Parse()
+
+	configuration := &config.ConfigurationStruct{}
+	dic := di.NewContainer(di.ServiceConstructorMap{
+		container.ConfigurationName: func(get di.Get) interface{} {
+			return configuration
+		},
+		bootstrapContainer.ConfigurationInterfaceName: func(get di.Get) interface{} {
+			return get(container.ConfigurationName)
+		},
+	})
+	bootstrap.Run(
+		configDir,
+		profileDir,
+		internal.ConfigFileName,
+		useRegistry,
+		clients.SecurityFileTokenProviderServiceKey,
+		configuration,
+		startupTimer,
+		dic,
+		[]interfaces.BootstrapHandler{
+			fileprovider.Handler,
+		},
+	)
+}
---
+++ internal/security/fileprovider/init.go
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * Copyright 2019 Dell Inc.
+ * Copyright 2019 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ *******************************************************************************/
+
+package fileprovider
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/edgexfoundry/edgex-go/internal"
+	bootstrapContainer "github.com/edgexfoundry/edgex-go/internal/pkg/bootstrap/container"
+	"github.com/edgexfoundry/edgex-go/internal/pkg/bootstrap/startup"
+	"github.com/edgexfoundry/edgex-go/internal/pkg/di"
+	"github.com/edgexfoundry/edgex-go/internal/security/authtokenloader"
+	"github.com/edgexfoundry/edgex-go/internal/security/fileioperformer"
+	"github.com/edgexfoundry/edgex-go/internal/security/fileprovider/container"
+	"github.com/edgexfoundry/edgex-go/internal/security/secretstoreclient"
+)
+
+// BootstrapHandler fulfills the BootstrapHandler contract and performs initialization needed by the data service.
+func Handler(ctx context.Context, wg *sync.WaitGroup, startupTimer startup.Timer, dic *di.Container) bool {
+	cfg := container.ConfigurationFrom(dic.Get)
+	loggingClient := bootstrapContainer.LoggingClientFrom(dic.Get)
+
+	fileOpener := fileioperformer.NewDefaultFileIoPerformer()
+	tokenProvider := authtokenloader.NewAuthTokenLoader(fileOpener)
+
+	var req internal.HttpCaller
+	if caFilePath := cfg.SecretService.CaFilePath; caFilePath != "" {
+		loggingClient.Info("using certificate verification for secret store connection")
+		caReader, err := fileOpener.OpenFileReader(caFilePath, os.O_RDONLY, 0400)
+		if err != nil {
+			loggingClient.Error(fmt.Sprintf("failed to load CA certificate: %s", err.Error()))
+			return false
+		}
+		req = secretstoreclient.NewRequestor(loggingClient).WithTLS(caReader, cfg.SecretService.ServerName)
+	} else {
+		loggingClient.Info("bypassing certificate verification for secret store connection")
+		req = secretstoreclient.NewRequestor(loggingClient).Insecure()
+	}
+	vaultScheme := cfg.SecretService.Scheme
+	vaultHost := fmt.Sprintf("%s:%v", cfg.SecretService.Server, cfg.SecretService.Port)
+	vaultClient := secretstoreclient.NewSecretStoreClient(loggingClient, req, vaultScheme, vaultHost)
+
+	fileProvider := NewTokenProvider(loggingClient, fileOpener, tokenProvider, vaultClient)
+
+	fileProvider.SetConfiguration(cfg.SecretService, cfg.TokenFileProvider)
+	err := fileProvider.Run()
+
+	if err != nil {
+		loggingClient.Error(fmt.Sprintf("error occurred generating tokens: %s", err.Error()))
+	}
+
+	return false // Tell bootstrap.Run() to exit wait loop and terminate
+}
