6878fc489309d9aec71e055d80521ac6cfaf60db
Ian Johnson <ian.johnson@canonical.com>
2019-12-11 11:52:38 -0600

    test-attribution-txt.sh: check for unused dependencies as well
    Also check that we don't have unnecessary attributions here as well, since devs
    can also remove dependencies that we don't need to attribute anymore.
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- bin/test-attribution-txt.sh
+++ bin/test-attribution-txt.sh
@@ -7,6 +7,10 @@ GIT_ROOT=$(dirname "$SCRIPT_DIR")
 
 EXIT_CODE=0
 
+# turn on nullglobbing so if there is nothing in cmd dir then we don't do
+# anything in the loops over cmd
+shopt -s nullglob
+
 cd "$GIT_ROOT"
 
 if [ -d vendor.bk ]; then
@@ -24,6 +28,11 @@ cleanup()
     if [ -d vendor.bk ]; then
         mv vendor.bk vendor
     fi
+    # remove any attribution dependency files from each of the cmd dirs
+    for cmd in cmd/* ; do
+        cd "$GIT_ROOT/$cmd"
+        rm -f dependency-*.bk
+    done
     exit $EXIT_CODE
 }
 
@@ -35,25 +44,45 @@ fi
 # create a vendor dir with the mod dependencies
 GO111MODULE=on go mod vendor
 
-# turn on nullglobbing so if there is nothing in cmd dir then we don't do
-# anything in this loop
-shopt -s nullglob
-
 for cmd in cmd/* ; do
     cd "$cmd"
-        if [ ! -f Attribution.txt ]; then
-            echo "An Attribution.txt file for $cmd is missing, please add"
-            EXIT_CODE=1
-        else
-            # loop over every library in the modules.txt file in vendor
-            while IFS= read -r lib; do
-                if ! grep -q "$lib" Attribution.txt; then 
-                    echo "An attribution for $lib is missing from in $cmd Attribution.txt, please add"
-                    # need to do this in a bash subshell, see SC2031
-                    (( EXIT_CODE=1 ))
+    if [ ! -f Attribution.txt ]; then
+        echo "An Attribution.txt file for $cmd is missing, please add"
+        EXIT_CODE=1
+    else
+        # loop over every library in the modules.txt file in vendor
+        while IFS= read -r lib; do
+            if ! grep -q "$lib" Attribution.txt; then 
+                echo "An attribution for $lib is missing from in $cmd Attribution.txt, please add"
+                # need to do this in a bash subshell, see SC2031
+                (( EXIT_CODE=1 ))
+            fi
+        done < <(grep '#' < "$GIT_ROOT/vendor/modules.txt" | awk '{print $2}')
+
+        # now check for stale, unused attributions
+        # we need to split the attribution.txt file by blank lines and put each
+        # dependency segment into it's own file for processing
+        tail -n +2 < Attribution.txt | awk -v RS= '{print > ("dependency-" NR ".bk")}' 
+        for dep in ./dependency-*.bk; do
+            # the format of the dependency is supposed to be
+            # PKG/NAME (LICENSE-TYPE) LIBRARY-LINK
+            # LICENSE-LINK
+            depName=$(head -n 1 "$dep" | awk '{print $1}')
+            if [ -z "$depName" ]; then
+                echo "invalid dependency format for $cmd/Attribution.txt"
+                EXIT_CODE=1
+                break
+            else
+                # check if the name of the library is in the modules, note that
+                # we don't use the link since the link could be ambiguous, but
+                # library names should always be unique
+                if ! grep "#" < "$GIT_ROOT/vendor/modules.txt" | awk '{print $2}' | grep -q "$depName"; then
+                    echo "Unnecessary attribution of $depName in $cmd/Attribution.txt, please remove"
+                    EXIT_CODE=1
                 fi
-            done < <(grep '#' < "$GIT_ROOT/vendor/modules.txt" | awk '{print $2}')
-        fi
+            fi
+        done
+    fi
     cd "$GIT_ROOT"
 done
 
