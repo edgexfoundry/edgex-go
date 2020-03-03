0f2a5fbcb1ddb3422469a5da2066fe06e4bf9b30
Michael Estrin <michaelestrin@users.noreply.github.com>
2020-03-03 08:34:13 -0600

    Look at Driving "Default Services List" via Configuration (#2401)
    Fixes: https://github.com/edgexfoundry/edgex-go/issues/1662
    Signed-off-by: Michael Estrin <m.estrin@dell.com>

--- internal/system/agent/getconfig/executor.go
+++ internal/system/agent/getconfig/executor.go
@@ -16,94 +16,32 @@ package getconfig
 
 import (
 	"context"
-	"fmt"
-	"sync"
 
-	"github.com/edgexfoundry/edgex-go/internal"
-	"github.com/edgexfoundry/edgex-go/internal/pkg/urlclient"
-	agentClients "github.com/edgexfoundry/edgex-go/internal/system/agent/clients"
-
-	bootstrapConfig "github.com/edgexfoundry/go-mod-bootstrap/config"
-
-	"github.com/edgexfoundry/go-mod-core-contracts/clients"
 	"github.com/edgexfoundry/go-mod-core-contracts/clients/general"
-	"github.com/edgexfoundry/go-mod-core-contracts/clients/logger"
-	"github.com/edgexfoundry/go-mod-registry/registry"
 )
+
+// clientFactory defines contract for creating/retrieving a general client.
+type clientFactory interface {
+	New(serviceName string) (general.GeneralClient, error)
+}
 
 // executor contains references to dependencies required to execute a get configuration request.
 type executor struct {
-	genClients      *agentClients.General
-	registryClient  registry.Client
-	loggingClient   logger.LoggingClient
-	serviceProtocol string
+	clientFactory clientFactory
 }
 
 // NewExecutor is a factory function that returns an initialized executor struct.
-func NewExecutor(
-	genClients *agentClients.General,
-	registryClient registry.Client,
-	lc logger.LoggingClient,
-	serviceProtocol string) *executor {
-
+func NewExecutor(clientFactory clientFactory) *executor {
 	return &executor{
-		genClients:      genClients,
-		registryClient:  registryClient,
-		loggingClient:   lc,
-		serviceProtocol: serviceProtocol,
+		clientFactory: clientFactory,
 	}
 }
 
 // Do fulfills the GetExecutor contract and implements the functionality to retrieve a service's configuration.
 func (e executor) Do(ctx context.Context, serviceName string) (string, error) {
-	var result string
-	client, ok := e.genClients.Get(serviceName)
-	if !ok {
-		if e.registryClient == nil {
-			return "", fmt.Errorf("registryClient not initialized; required to handle unknown service: %s", serviceName)
-		}
-
-		// Service unknown to SMA, so ask the Registry whether `serviceName` is available.
-		ok, err := e.registryClient.IsServiceAvailable(serviceName)
-		if err != nil {
-			return "", err
-		}
-		if !ok {
-			return "", fmt.Errorf("service %s is not available", serviceName)
-		}
-
-		e.loggingClient.Info(fmt.Sprintf("Registry responded with %s available", serviceName))
-
-		// Since serviceName is unknown to SMA, ask the Registry for a ServiceEndpoint associated with `serviceName`
-		ep, err := e.registryClient.GetServiceEndpoint(serviceName)
-		if err != nil {
-			return "", fmt.Errorf("on attempting to get ServiceEndpoint for %s, got error: %v", serviceName, err.Error())
-		}
-
-		configClient := bootstrapConfig.ClientInfo{
-			Protocol: e.serviceProtocol,
-			Host:     ep.Host,
-			Port:     ep.Port,
-		}
-
-		// Add the serviceName key to the map where the value is the respective GeneralClient
-		client = general.NewGeneralClient(
-			urlclient.New(
-				ctx,
-				&sync.WaitGroup{},
-				e.registryClient,
-				ep.ServiceId,
-				"/",
-				internal.ClientMonitorDefault,
-				configClient.Url()+clients.ApiConfigRoute,
-			),
-		)
-		e.genClients.Set(ep.ServiceId, client)
-	}
-
-	result, err := client.FetchConfiguration(ctx)
+	client, err := e.clientFactory.New(serviceName)
 	if err != nil {
 		return "", err
 	}
-	return result, nil
+	return client.FetchConfiguration(ctx)
 }
