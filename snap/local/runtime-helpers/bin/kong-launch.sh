45d541d30cdfeeadaf160443931a89cd0ea601d5
Tony Espy <espy@canonical.com>
2020-05-01 09:45:00 -0400

    snap: enforce postgresql password auth
    This commit enforces postgresql password-based authentication.
    A random password is generated at install time via 'apg', and
    is persisted to disk. This file is read by the kong-launch.sh
    script, and used to set the KONG_PW_PASSWORD environment var.
    
    Fixes: #1943
    
    Signed-off-by: Tony Espy <espy@canonical.com>

--- snap/local/runtime-helpers/bin/kong-launch.sh
+++ snap/local/runtime-helpers/bin/kong-launch.sh
@@ -8,6 +8,9 @@ export LUA_VERSION=5.1
 export LUA_PATH="$SNAP/lualib/?.lua;$SNAP/lualib/?/init.lua;$SNAP/usr/share/lua/$LUA_VERSION/?.lua;$SNAP/usr/share/lua/$LUA_VERSION/?/init.lua;$SNAP/lib/lua/$LUA_VERSION/?.lua;$SNAP/lib/lua/$LUA_VERSION/?/init.lua;$SNAP/share/lua/$LUA_VERSION/?.lua;$SNAP/share/lua/$LUA_VERSION/?/init.lua;;"
 export LUA_CPATH="$SNAP/lualib/?.so;$SNAP/lib/lua/$LUA_VERSION/?.so;$SNAP/lib/$ARCH_LIB_NAME/lua/$LUA_VERSION/?.so;;"
 
+# set postgresql password
+export KONG_PG_PASSWORD=`cat "$SNAP_DATA/config/postgres/kongpw"`
+
 # also make sure our logs dir exists because for some reason nginx can't create
 # it's own logs dir
 mkdir -p "$KONG_LOGS_DIR"
