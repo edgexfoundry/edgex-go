152b5186ab7fa561a906ef03a5d16687ab3020a7
Tony Espy <espy@canonical.com>
2020-05-05 18:04:18 -0400

    snap: update support-rulesengine wrapper
    Update support-rulesengine wrapper script to
    only use local configuration (i.e. no consul),
    and to also use the symbolic link in $SNAP/bin
    instead of using a jdk-specific path.
    
    Signed-off-by: Tony Espy <espy@canonical.com>

--- snap/local/runtime-helpers/bin/support-rulesengine-wrapper.sh
+++ snap/local/runtime-helpers/bin/support-rulesengine-wrapper.sh
@@ -1,18 +1,5 @@
 #!/bin/bash -e
 
-if [ "$(arch)" = "aarch64" ] ; then
-    ARCH="arm64"
-elif [ "$(arch)" = "x86_64" ] ; then
-    ARCH="amd64"
-else
-    echo "Unsupported architecture: $(arch)"
-    exit 1
-fi
-
-JAVA="$SNAP"/usr/lib/jvm/java-8-openjdk-"$ARCH"/jre/bin/java
-
-$JAVA -jar -Djava.security.egd=file:/dev/urandom -Xmx100M \
-            -Dspring.cloud.consul.enabled=true \
-            -Dspring.cloud.consul.host=localhost \
-            -Dlogging.file="$SNAP_COMMON/logs/edgex-support-rulesengine.log" \
-            "$SNAP/jar/support-rulesengine/support-rulesengine.jar"
+"$SNAP"/bin/java -jar -Djava.security.egd=file:/dev/urandom -Xmx100M \
+            -Dlogging.file="$SNAP_COMMON"/logs/edgex-support-rulesengine.log \
+            "$SNAP"/jar/support-rulesengine/support-rulesengine.jar
