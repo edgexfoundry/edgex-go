a8cf1919c48a242a38eb9727bbefae72bdb47235
Tony Espy <espy@canonical.com>
2020-05-13 12:38:54 -0400

    snap: fix support-rulesengine
    This commit fixes a couple of problems with the
    legacy (deprecated) support-rulesengine:
    
     - restore openjdk-8
     - update the wrapper script to disable consul
     - fix configure hook enable logic
    
    Signed-off-by: Tony Espy <espy@canonical.com>

--- snap/local/runtime-helpers/bin/support-rulesengine-wrapper.sh
+++ snap/local/runtime-helpers/bin/support-rulesengine-wrapper.sh
@@ -1,5 +1,18 @@
 #!/bin/bash -e
 
-"$SNAP"/bin/java -jar -Djava.security.egd=file:/dev/urandom -Xmx100M \
+if [ "$(arch)" = "aarch64" ] ; then
+    ARCH="arm64"
+elif [ "$(arch)" = "x86_64" ] ; then
+    ARCH="amd64"
+else
+    echo "Unsupported architecture: $(arch)"
+    exit 1
+fi
+
+JAVA="$SNAP"/usr/lib/jvm/java-8-openjdk-"$ARCH"/jre/bin/java
+
+$JAVA -jar -Djava.security.egd=file:/dev/urandom -Xmx100M \
             -Dlogging.file="$SNAP_COMMON"/logs/edgex-support-rulesengine.log \
-            "$SNAP"/jar/support-rulesengine/support-rulesengine.jar
+            -Drules.default.path="$SNAP_DATA"/support-rulesengine/rules \
+            -Drules.template.path="$SNAP_DATA"/support-rulesengine/templates \
+            "$SNAP/jar/support-rulesengine/support-rulesengine.jar"
