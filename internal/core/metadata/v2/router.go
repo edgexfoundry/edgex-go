4c1a6d2ffbdc3154b41bea1622b22c22abf998aa
Cloud Tsai <cloudxxx8@gmail.com>
2020-10-03 00:31:35 +0800

    feat(v2 all):Impl common V2 API (#2746)
    Create the controller for common V2 API, including ping, metrics,
    version, and config.
    Add the API to the core-data and core-metadata router.
    
    Fix https://github.com/edgexfoundry/edgex-go/issues/2721
    
    Signed-off-by: Cloud Tsai <cloudxxx8@gmail.com>

--- internal/core/metadata/v2/router.go
+++ internal/core/metadata/v2/router.go
@@ -3,10 +3,12 @@ package v2
 import (
 	"net/http"
 
-	httpController "github.com/edgexfoundry/edgex-go/internal/core/metadata/v2/controller/http"
+	dpController "github.com/edgexfoundry/edgex-go/internal/core/metadata/v2/controller/http"
 	"github.com/edgexfoundry/edgex-go/internal/pkg/correlation"
+	commonController "github.com/edgexfoundry/edgex-go/internal/pkg/v2/controller/http"
 
 	"github.com/edgexfoundry/go-mod-bootstrap/di"
+
 	v2Constant "github.com/edgexfoundry/go-mod-core-contracts/v2"
 
 	"github.com/gorilla/mux"
@@ -14,12 +16,18 @@ import (
 
 func LoadRestRoutes(r *mux.Router, dic *di.Container) {
 	// v2 API routes
-	// Device Profile
-	dc := httpController.NewDeviceProfileController(dic)
+	// Common
+	cc := commonController.NewV2CommonController(dic)
+	r.HandleFunc(v2Constant.ApiPingRoute, cc.Ping).Methods(http.MethodGet)
+	r.HandleFunc(v2Constant.ApiVersionRoute, cc.Version).Methods(http.MethodGet)
+	r.HandleFunc(v2Constant.ApiConfigRoute, cc.Config).Methods(http.MethodGet)
+	r.HandleFunc(v2Constant.ApiMetricsRoute, cc.Metrics).Methods(http.MethodGet)
 
+	// Device Profile
+	dc := dpController.NewDeviceProfileController(dic)
 	r.HandleFunc(v2Constant.ApiDeviceProfileRoute, dc.AddDeviceProfile).Methods(http.MethodPost)
 	dp := r.PathPrefix(v2Constant.ApiDeviceProfileRoute).Subrouter()
-	dp.HandleFunc(httpController.UploadFile, dc.AddDeviceProfileByYaml).Methods(http.MethodPost)
+	dp.HandleFunc(dpController.UploadFile, dc.AddDeviceProfileByYaml).Methods(http.MethodPost)
 
 	r.Use(correlation.ManageHeader)
 	r.Use(correlation.OnResponseComplete)
