dcbd5402a1caf5f1df377140b814aa7d9b449746
weichou <weichou1229@gmail.com>
2020-10-23 14:13:32 +0800

    feat(data): Implement GET /event/device/name/{name} V2 API
    Implement CoreData GET /event/device/name/{name} API to query events by offset, limit and device name.
    
    Fix #2842
    
    Signed-off-by: weichou <weichou1229@gmail.com>

--- internal/core/data/v2/router.go
+++ internal/core/data/v2/router.go
@@ -31,6 +31,7 @@ func LoadRestRoutes(r *mux.Router, dic *di.Container) {
 	r.HandleFunc(v2Constant.ApiEventCountByDeviceRoute, ec.EventCountByDevice).Methods(http.MethodGet)
 	r.HandleFunc(v2Constant.ApiEventPushRoute, ec.UpdateEventPushedById).Methods(http.MethodPut)
 	r.HandleFunc(v2Constant.ApiAllEventRoute, ec.AllEvents).Methods(http.MethodGet)
+	r.HandleFunc(v2Constant.ApiEventByDeviceNameRoute, ec.EventsByDeviceName).Methods(http.MethodGet)
 
 	r.Use(correlation.ManageHeader)
 	r.Use(correlation.OnResponseComplete)
