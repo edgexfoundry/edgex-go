02b706ffd2e2a3d13f341e115d0dd90d2a274cf2
Tony Espy <espy@canonical.com>
2021-02-16 18:03:07 -0500

    fix(snap): use build-snaps for go
    This change updates the snap build to use the go
    snap (via build-snaps) instead of downloading the
    go binary release tarballs.
    
    Signed-off-by: Tony Espy <espy@canonical.com>

--- snap/local/build-helpers/bin/go-build-helper.sh
+++ snap/local/build-helpers/bin/go-build-helper.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 #
 # $1 - go import path
-# $2 - maj/min version of go; must correspond to a go part in the snap
 #
 # example usage:
-# $ gopartbootstrap github.com/edgexfoundry/edgex-go 1.15
+# $ gopartbootstrap github.com/edgexfoundry/edgex-go
 gopartbootstrap() 
 {
     # first set the GOPATH to be in the current directory and in ".gopath"
     GOPATH="$(pwd)/.gopath"
     export GOPATH
+
     # setup path to include both $SNAPCRAFT_STAGE/bin and $GOPATH/bin
     # the former is for the go tools, as well as things like glide, etc.
     # while the later is for govendor, etc. and other go tools that might need to be installed
-    export PATH="$SNAPCRAFT_STAGE/go$2/bin:$GOPATH/bin:$PATH"
-    # set GOROOT to be whatever the go tool from SNAPCRAFT_STAGE/bin is
-    GOROOT=$(go env GOROOT)
-    export GOROOT
+    export PATH="$GOPATH/bin:$PATH"
+
     # now setup the GOPATH for this part using the import path
     export GOIMPORTPATH="$GOPATH/src/$1"
     mkdir -p "$GOIMPORTPATH"
