76edbf6b374c198a59e0ea3df6d90d6fbb7c8c41
Felix Ting <43240457+FelixTing@users.noreply.github.com>
2021-03-03 01:56:33 +0800

    feat(notifications): Implement GET /notification/category/{category} V2 API (#3212)
    Per https://app.swaggerhub.com/apis-docs/EdgeXFoundry1/support-notifications/2.x#/default/get_notification_category__category_, implemented GET /notification/category/{category} V2 API.
    
    Signed-off-by: Felix Ting <felix@iotechsys.com>

--- internal/support/notifications/v2/controller/http/notification.go
+++ internal/support/notifications/v2/controller/http/notification.go
@@ -6,20 +6,27 @@
 package http
 
 import (
+	"math"
 	"net/http"
 
 	"github.com/edgexfoundry/edgex-go/internal/pkg"
 	"github.com/edgexfoundry/edgex-go/internal/pkg/correlation"
 	"github.com/edgexfoundry/edgex-go/internal/pkg/v2/utils"
+	notificationContainer "github.com/edgexfoundry/edgex-go/internal/support/notifications/container"
 	"github.com/edgexfoundry/edgex-go/internal/support/notifications/v2/application"
 	"github.com/edgexfoundry/edgex-go/internal/support/notifications/v2/io"
 
 	"github.com/edgexfoundry/go-mod-bootstrap/v2/bootstrap/container"
 	"github.com/edgexfoundry/go-mod-bootstrap/v2/di"
-	"github.com/edgexfoundry/go-mod-core-contracts/v2/clients"
 
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/clients"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/errors"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/v2"
 	commonDTO "github.com/edgexfoundry/go-mod-core-contracts/v2/v2/dtos/common"
 	requestDTO "github.com/edgexfoundry/go-mod-core-contracts/v2/v2/dtos/requests"
+	responseDTO "github.com/edgexfoundry/go-mod-core-contracts/v2/v2/dtos/responses"
+
+	"github.com/gorilla/mux"
 )
 
 type NotificationController struct {
@@ -35,17 +42,17 @@ func NewNotificationController(dic *di.Container) *NotificationController {
 	}
 }
 
-func (sc *NotificationController) AddNotification(w http.ResponseWriter, r *http.Request) {
+func (nc *NotificationController) AddNotification(w http.ResponseWriter, r *http.Request) {
 	if r.Body != nil {
 		defer func() { _ = r.Body.Close() }()
 	}
 
-	lc := container.LoggingClientFrom(sc.dic.Get)
+	lc := container.LoggingClientFrom(nc.dic.Get)
 
 	ctx := r.Context()
 	correlationId := correlation.FromContext(ctx)
 
-	addNotificationDTOs, err := sc.reader.ReadAddNotificationRequest(r.Body)
+	addNotificationDTOs, err := nc.reader.ReadAddNotificationRequest(r.Body)
 	if err != nil {
 		lc.Error(err.Error(), clients.CorrelationHeader, correlationId)
 		lc.Debug(err.DebugMessages(), clients.CorrelationHeader, correlationId)
@@ -63,7 +70,7 @@ func (sc *NotificationController) AddNotification(w http.ResponseWriter, r *http
 	for i, n := range notifications {
 		var response interface{}
 		reqId := addNotificationDTOs[i].RequestId
-		newId, err := application.AddNotification(n, ctx, sc.dic)
+		newId, err := application.AddNotification(n, ctx, nc.dic)
 		if err != nil {
 			lc.Error(err.Error(), clients.CorrelationHeader, correlationId)
 			lc.Debug(err.DebugMessages(), clients.CorrelationHeader, correlationId)
@@ -83,4 +90,42 @@ func (sc *NotificationController) AddNotification(w http.ResponseWriter, r *http
 
 	utils.WriteHttpHeader(w, ctx, http.StatusMultiStatus)
 	pkg.Encode(addResponses, w, lc)
+}
+
+func (nc *NotificationController) NotificationsByCategory(w http.ResponseWriter, r *http.Request) {
+	lc := container.LoggingClientFrom(nc.dic.Get)
+	ctx := r.Context()
+	correlationId := correlation.FromContext(ctx)
+	config := notificationContainer.ConfigurationFrom(nc.dic.Get)
+
+	vars := mux.Vars(r)
+	category := vars[v2.Category]
+
+	var response interface{}
+	var statusCode int
+
+	// parse URL query string for offset, limit
+	offset, limit, _, err := utils.ParseGetAllObjectsRequestQueryString(r, 0, math.MaxInt32, -1, config.Service.MaxResultCount)
+	if err != nil {
+		lc.Error(err.Error(), clients.CorrelationHeader, correlationId)
+		lc.Debug(err.DebugMessages(), clients.CorrelationHeader, correlationId)
+		response = commonDTO.NewBaseResponse("", err.Message(), err.Code())
+		statusCode = err.Code()
+	} else {
+		notifications, err := application.NotificationsByCategory(offset, limit, category, nc.dic)
+		if err != nil {
+			if errors.Kind(err) != errors.KindEntityDoesNotExist {
+				lc.Error(err.Error(), clients.CorrelationHeader, correlationId)
+			}
+			lc.Debug(err.DebugMessages(), clients.CorrelationHeader, correlationId)
+			response = commonDTO.NewBaseResponse("", err.Message(), err.Code())
+			statusCode = err.Code()
+		} else {
+			response = responseDTO.NewMultiNotificationsResponse("", "", http.StatusOK, notifications)
+			statusCode = http.StatusOK
+		}
+	}
+
+	utils.WriteHttpHeader(w, ctx, statusCode)
+	pkg.Encode(response, w, lc)
 }
