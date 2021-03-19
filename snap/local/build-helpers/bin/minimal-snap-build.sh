3a20104d6d86acd1d96608b3f5976dccc81e9b59
Ernesto Ojeda <ernesto.ojeda@intel.com>
2021-03-19 08:12:27 -0700

    ci: Trying a local yq
    Signed-off-by: Ernesto Ojeda <ernesto.ojeda@intel.com>

--- snap/local/build-helpers/bin/minimal-snap-build.sh
+++ snap/local/build-helpers/bin/minimal-snap-build.sh
@@ -13,34 +13,41 @@
 # work with the patch applied). This further reduces the build
 # time.
 
-# echo "====================================================="
-# echo "SNAP DEBUG"
-# env | sort
-# mount
-# df -h
-# echo "====================================================="
-# ls -alR /w
-# ls -al $(pwd)/snap || true
-# echo "====================================================="
+echo "====================================================="
+echo "SNAP DEBUG"
+env | sort
+mount
+df -h
+echo "====================================================="
+ls -alR /w
+ls -al $(pwd)/snap || true
+echo "====================================================="
 
-sudo snap install yq --devmode --channel=v4/stable
+# sudo snap install yq --devmode --channel=v4/stable
+
+curl -L --output yq.tar.gz https://github.com/mikefarah/yq/releases/download/v4.6.2/yq_linux_amd64.tar.gz
+tar -xvf yq.tar.gz
+mv yq_linux_amd64 yq
+sudo snap remove yq 
+
+# remove first chunk of apps
 
 CURRDIR=$(pwd)
 SNAPCRAFT_YAML="$CURRDIR/snap/snapcraft.yaml"
 
 # remove first chunk of apps
-sudo yq e -P -i 'del(.apps.consul,.apps.redis,.apps.postgres,.apps.kong-daemon,.apps.vault,.apps.vault-cli)' "$SNAPCRAFT_YAML"
+$CURRDIR/yq e -P -i 'del(.apps.consul,.apps.redis,.apps.postgres,.apps.kong-daemon,.apps.vault,.apps.vault-cli)' "$SNAPCRAFT_YAML"
 
 # remove second chunk of apps
-sudo yq e -P -i 'del(.apps.device-virtual,.apps.app-service-configurable)' "$SNAPCRAFT_YAML"
+$CURRDIR/yq e -P -i 'del(.apps.device-virtual,.apps.app-service-configurable)' "$SNAPCRAFT_YAML"
 
 # remove third chunk of apps
-sudo yq e -P -i 'del(.apps.redis-cli,.apps.consul-cli)' "$SNAPCRAFT_YAML"
+$CURRDIR/yq e -P -i 'del(.apps.redis-cli,.apps.consul-cli)' "$SNAPCRAFT_YAML"
 
 # remove fourth chunk of apps
-sudo yq e -P -i 'del(.apps.kong,.apps.psql,.apps.psql-any,.apps.createdb,.apps.kuiper,.apps.kuiper-cli)' "$SNAPCRAFT_YAML"
+$CURRDIR/yq e -P -i 'del(.apps.kong,.apps.psql,.apps.psql-any,.apps.createdb,.apps.kuiper,.apps.kuiper-cli)' "$SNAPCRAFT_YAML"
 
 # remove unwanted parts
-sudo yq e -P -i 'del(.parts.snapcraft-preload,.parts.postgres,.parts.consul,.parts.redis,.parts.kong,.parts.vault,.parts.device-virtual-go,.parts.kuiper)' "$SNAPCRAFT_YAML"
+$CURRDIR/yq e -P -i 'del(.parts.snapcraft-preload,.parts.postgres,.parts.consul,.parts.redis,.parts.kong,.parts.vault,.parts.device-virtual-go,.parts.kuiper)' "$SNAPCRAFT_YAML"
 
 
