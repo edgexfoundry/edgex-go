9dcd56cb3b9562e2b1febbaf3d413392552234ad
edgego <80612608+edgego@users.noreply.github.com>
2022-02-03 17:48:04 +0800

    Update sender.go

--- internal/support/notifications/application/channel/sender.go
+++ internal/support/notifications/application/channel/sender.go
@@ -14,7 +14,110 @@ import (
 
 	"github.com/edgexfoundry/go-mod-core-contracts/v2/errors"
 	"github.com/edgexfoundry/go-mod-core-contracts/v2/models"
-)
+)// DingdingSender is the implementation of the interfaces.ChannelSender, which is used to send the notifications via REST
+// add  by edgeGo
+type DingdingSender struct {
+	dic *di.Container
+}
+
+// AliSmsSender is the implementation of the interfaces.ChannelSender, which is used to send the notifications via REST
+// add  by edgeGo
+type AliSmsSender struct {
+	dic *di.Container
+}
+
+// NewSmsSender creates the SmsSender instance
+// add  by edgeGo
+func NewSmsSender(dic *di.Container) Sender {
+	return &AliSmsSender{dic: dic}
+}
+
+// NewDingdingSender creates the DingdingSender instance
+// add  by edgeGo
+func NewDingdingSender(dic *di.Container) Sender {
+	return &DingdingSender{dic: dic}
+}
+
+// NewRESTSender creates the RESTSender instance
+func NewRESTSender(dic *di.Container) Sender {
+	return &RESTSender{dic: dic}
+}
+
+// Send sms request to the aliyun clound sms service address,add by edgeGo
+func (sender *AliSmsSender) Send(notification models.Notification, address models.Address) (res string, err errors.EdgeX) {
+	//lc := container.LoggingClientFrom(sender.dic.Get)
+	smsInfo := notificationContainer.ConfigurationFrom(sender.dic.Get).SmsInfo
+
+	smsAddress, ok := address.(models.SmsAddress)
+	if !ok {
+		return "", errors.NewCommonEdgeX(errors.KindContractInvalid, "fail to cast Address to SmsAddress", nil)
+	}
+
+	client, e := dysmsapi.NewClientWithAccessKey(smsInfo.Regin, smsInfo.AccessKey, smsInfo.Secret)
+	if e != nil {
+		return e.Error(), errors.NewCommonEdgeXWrapper(e)
+	}
+
+	phoneNumberJson := "["
+	for i := 0; i < len(smsAddress.Recipients); i++ {
+		if i != len(smsAddress.Recipients)-1 {
+			phoneNumberJson += "\"" + smsAddress.Recipients[i] + "\","
+		} else {
+			phoneNumberJson += "\"" + smsAddress.Recipients[i] + "\"]"
+		}
+	}
+
+	request := dysmsapi.CreateSendBatchSmsRequest()
+	request.Scheme = "https"
+	request.PhoneNumberJson = phoneNumberJson
+	request.SignNameJson = "EdgeGo"
+	request.TemplateCode = "SMS_0000"
+	request.TemplateParamJson = ""
+
+	response, e := client.SendBatchSms(request)
+	if e != nil {
+		return e.Error(), errors.NewCommonEdgeXWrapper(e)
+	}
+
+	return response.String(), nil
+}
+
+//add by edgeGo
+func sign(t int64, secret string) string {
+	secStr := fmt.Sprintf("%d\n%s", t, secret)
+	hmac256 := hmac.New(sha256.New, []byte(secret))
+	hmac256.Write([]byte(secStr))
+	result := hmac256.Sum(nil)
+	return base64.StdEncoding.EncodeToString(result)
+}
+
+// Send dingding request to the webhook url ,add by edgeGo
+func (sender *DingdingSender) Send(notification models.Notification, address models.Address) (res string, err errors.EdgeX) {
+	dingInfo := notificationContainer.ConfigurationFrom(sender.dic.Get).DingdingInfo
+
+	dingAddress, ok := address.(models.DingdingAddress)
+	if !ok {
+		return "", errors.NewCommonEdgeX(errors.KindContractInvalid, "fail to cast Address to DingdingAddress", nil)
+	}
+
+	timestamp := time.Now().UnixNano() / 1e6
+	signStr := sign(timestamp, dingAddress.Secret)
+	dingUrl := fmt.Sprintf("%s?access_token=%s&timestamp=%d&sign=%s", dingInfo.Webhook, dingAddress.AccessToken, timestamp, signStr)
+
+	resp, e := http.Post(dingUrl, "application/json", strings.NewReader(string(notification.Content)))
+	if e != nil {
+		return e.Error(), errors.NewCommonEdgeXWrapper(e)
+	}
+	defer resp.Body.Close()
+
+	body, e := ioutil.ReadAll(resp.Body)
+	if e != nil {
+		return e.Error(), errors.NewCommonEdgeXWrapper(e)
+	}
+
+	return string(body), nil
+}
+
 
 // Sender abstracts the notification sending via specified channel
 type Sender interface {
@@ -25,6 +128,7 @@ type Sender interface {
 type RESTSender struct {
 	dic *di.Container
 }
+
 
 // NewRESTSender creates the RESTSender instance
 func NewRESTSender(dic *di.Container) Sender {
