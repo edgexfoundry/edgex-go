e6a6a81dc368a2a1e05de6f3aee8cce92d1acb8d
Valina Li <valina.li@intel.com>
2022-07-13 17:55:29 -0700

    fix: security-secretstore-setup volume init semantics
    fixes: #3852
    Signed-off-by: Valina Li <valina.li@intel.com>

--- cmd/security-secretstore-setup/entrypoint.sh
+++ cmd/security-secretstore-setup/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/dumb-init /bin/sh
 #  ----------------------------------------------------------------------------------
-#  Copyright (c) 2020 Intel Corporation
+#  Copyright (c) 2022 Intel Corporation
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -20,6 +20,9 @@
 set -e
 
 # env settings are populated from env files of docker-compose
+
+# make config vault asset directory for case of helm
+mkdir -p /vault/config/assets
 
 echo "Initializing secret store..."
 /security-secretstore-setup --vaultInterval=10
