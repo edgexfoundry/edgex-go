c8dc4aaa282fbe6e844314843563128f08055c6c
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2022-10-26 17:54:56 +0200

    build(snap): kong migrations up before Kong startup (#4223)
    Signed-off-by: Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>

--- snap/local/runtime-helpers/bin/kong-daemon.sh
+++ snap/local/runtime-helpers/bin/kong-daemon.sh
@@ -2,14 +2,17 @@
 
 KONG=/usr/local/bin/kong
 
-# run kong migrations up to bootstrap the cassandra database
-# note that sometimes cassandra can be in a "starting up" state, etc.
+# bootstrap the database
+# note that sometimes the database can be in a "starting up" state, etc.
 # and in this case we should just loop and keep trying
 # we don't implement a timeout here because systemd will kill us if we 
 # don't succeed in 15 minutes (or whatever the configured stop-timeout is)
 until "$KONG" migrations bootstrap --conf "$KONG_CONF"; do
     sleep 5
 done
+
+# perform migration for upgrades
+"$KONG" migrations up --conf "$KONG_CONF"
 
 # set up Kong's admin API plugins via kong.yml:
 "$KONG" config db_import "$KONGADMIN_CONFIGFILEPATH" || true
