f88f776b66bfda32466101e5a93e8ee6f15bd1c4
Bryon Nevis <bryon.nevis@intel.com>
2023-01-13 12:45:41 -0800

    feat(security)!: Remove OAuth support for Kong users
    BREAKING CHANGE: Remove code to create an Oauth2 user in Kong
    
    Signed-off-by: Bryon Nevis <bryon.nevis@intel.com>

--- internal/security/config/command/proxy/adduser/command.go
+++ internal/security/config/command/proxy/adduser/command.go
@@ -107,9 +107,6 @@ func NewCommand(
 	if cmd.tokenType == interfaces.JwtTokenType && cmd.publicKeyPath == "" {
 		return nil, fmt.Errorf("%s proxy adduser: argument --public_key is required", os.Args[0])
 	}
-	if cmd.tokenType == interfaces.OAuth2TokenType && cmd.redirectUris == "" {
-		return nil, fmt.Errorf("%s proxy adduser: argument --redirect_uris is required", os.Args[0])
-	}
 	if cmd.jwt == "" {
 		return nil, fmt.Errorf("%s proxy adduser: argument --jwt is required", os.Args[0])
 	}
@@ -285,84 +282,6 @@ func (c *cmd) ExecuteAddJwt() (int, error) {
 	outputKey := fmt.Sprintf("%s", parsedResponse["key"])
 
 	fmt.Printf("%s\n", outputKey)
-
-	return interfaces.StatusCodeExitNormal, nil
-}
-
-// ExecuteAddOAuth2 (placeholder)
-func (c *cmd) ExecuteAddOAuth2() (statusCode int, err error) {
-
-	// Create a custom consumer in Kong
-	if err := c.createConsumer(); err != nil {
-		return interfaces.StatusCodeExitWithError, err
-	}
-
-	// Assign custom consumer to a group in Kong
-	if err := c.addUserToGroup(); err != nil {
-		return interfaces.StatusCodeExitWithError, err
-	}
-
-	// Create an OAuth application
-	// https://docs.konghq.com/hub/kong-inc/oauth2/#create-an-application
-
-	form := url.Values{
-		"name":          []string{c.username}, // use username as application name
-		"redirect_uris": []string{c.redirectUris},
-	}
-
-	// Client ID and client secret are auto-generated if not supplied
-	if len(c.clientID) > 0 {
-		form.Set("client_id", c.clientID)
-	}
-	if len(c.clientID) > 0 {
-		form.Set("client_secret", c.clientSecret)
-	}
-
-	kongURL := strings.Join([]string{c.configuration.KongURL.GetSecureURL(), "consumers", c.username, "oauth2"}, "/")
-	c.loggingClient.Infof("creating oauth application at %s", kongURL)
-
-	formVal := form.Encode()
-	req, err := http.NewRequest(http.MethodPost, kongURL, strings.NewReader(formVal))
-	if err != nil {
-		return interfaces.StatusCodeExitWithError, fmt.Errorf("failed to prepare request to create oauth application %s: %w", c.username, err)
-	}
-	req.Header.Add(common.ContentType, proxyCommon.UrlEncodedForm)
-	req.Header.Add(internal.AuthHeaderTitle, internal.BearerLabel+c.jwt)
-	resp, err := c.client.Do(req)
-	if err != nil {
-		return interfaces.StatusCodeExitWithError, fmt.Errorf("failed to send request to create oauth application %s: %w", c.username, err)
-	}
-	defer resp.Body.Close()
-
-	responseBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return interfaces.StatusCodeExitWithError, err
-	}
-
-	var parsedResponse map[string]interface{}
-
-	switch resp.StatusCode {
-	case http.StatusOK:
-	case http.StatusCreated:
-		if err := json.NewDecoder(bytes.NewReader(responseBody)).Decode(&parsedResponse); err != nil {
-			return interfaces.StatusCodeExitWithError, fmt.Errorf("unable to parse associate JWT response: %w", err)
-		}
-	case http.StatusConflict:
-		return interfaces.StatusCodeExitWithError, fmt.Errorf("associate JWT request failed (likely due to duplicate ID) with code: %d", resp.StatusCode)
-	default:
-		return interfaces.StatusCodeExitWithError, fmt.Errorf("associate JWT request failed with code: %d", resp.StatusCode)
-	}
-
-	clientID := fmt.Sprintf("%s", parsedResponse["client_id"])
-	clientSecret := fmt.Sprintf("%s", parsedResponse["client_secret"])
-
-	err = json.NewEncoder(os.Stdout).Encode(map[string]interface{}{
-		"client_id":     clientID,
-		"client_secret": clientSecret,
-	})
-	if err != nil {
-		return interfaces.StatusCodeExitWithError, fmt.Errorf("marshaling of client id and secret failed")
-	}
 
 	return interfaces.StatusCodeExitNormal, nil
 }
