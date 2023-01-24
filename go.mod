0d8c37666fe601bddb386208d8b3b7c5f58753b4
Elizabeth J Lee <elizabeth.j.lee@intel.com>
2023-01-24 12:08:30 -0700

    feat: Push common config to config provider
    Signed-off-by: Elizabeth J Lee <elizabeth.j.lee@intel.com>

--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module github.com/edgexfoundry/edgex-go
 require (
 	bitbucket.org/bertimus9/systemstat v0.0.0-20180207000608-0eeff89b0690
 	github.com/eclipse/paho.mqtt.golang v1.4.2
-	github.com/edgexfoundry/go-mod-bootstrap/v3 v3.0.0-dev.12
+	github.com/edgexfoundry/go-mod-bootstrap/v3 v3.0.0-dev.16
+	github.com/edgexfoundry/go-mod-configuration/v3 v3.0.0-dev.2
 	github.com/edgexfoundry/go-mod-core-contracts/v3 v3.0.0-dev.6
 	github.com/edgexfoundry/go-mod-messaging/v3 v3.0.0-dev.4
-	github.com/edgexfoundry/go-mod-secrets/v3 v3.0.0-dev.4
+	github.com/edgexfoundry/go-mod-secrets/v3 v3.0.0-dev.5
 	github.com/fxamacker/cbor/v2 v2.4.0
 	github.com/golang-jwt/jwt/v4 v4.4.3
 	github.com/gomodule/redigo v1.8.9
@@ -27,7 +28,6 @@ require (
 	github.com/armon/go-metrics v0.3.10 // indirect
 	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/edgexfoundry/go-mod-configuration/v3 v3.0.0-dev.2 // indirect
 	github.com/edgexfoundry/go-mod-registry/v3 v3.0.0-dev.3 // indirect
 	github.com/fatih/color v1.9.0 // indirect
 	github.com/go-kit/log v0.2.1 // indirect
--- internal/core/common_config/main.go
+++ internal/core/common_config/main.go
@@ -16,19 +16,29 @@ package common_config
 
 import (
 	"context"
+	"fmt"
+	"github.com/edgexfoundry/go-mod-bootstrap/v3/bootstrap/config"
 	"github.com/edgexfoundry/go-mod-bootstrap/v3/bootstrap/container"
 	"github.com/edgexfoundry/go-mod-bootstrap/v3/bootstrap/environment"
 	"github.com/edgexfoundry/go-mod-bootstrap/v3/bootstrap/flags"
 	"github.com/edgexfoundry/go-mod-bootstrap/v3/bootstrap/secret"
 	"github.com/edgexfoundry/go-mod-bootstrap/v3/bootstrap/startup"
 	"github.com/edgexfoundry/go-mod-bootstrap/v3/di"
+	"github.com/edgexfoundry/go-mod-configuration/v3/configuration"
+	"github.com/edgexfoundry/go-mod-configuration/v3/pkg/types"
 	"github.com/edgexfoundry/go-mod-core-contracts/v3/clients/logger"
 	"github.com/edgexfoundry/go-mod-core-contracts/v3/common"
 	"github.com/edgexfoundry/go-mod-core-contracts/v3/models"
+	"gopkg.in/yaml.v3"
 	"os"
 	"os/signal"
+	"sort"
 	"sync"
 	"syscall"
+)
+
+const (
+	commonConfigDone = "isCommonConfigReady"
 )
 
 func Main(ctx context.Context, cancel context.CancelFunc) {
@@ -56,7 +66,6 @@ func Main(ctx context.Context, cancel context.CancelFunc) {
 			return lc
 		},
 	})
-
 	secretProvider, err := secret.NewSecretProvider(nil, environment.NewVariables(lc), ctx, startupTimer, dic, common.CoreCommonConfigServiceKey)
 	if err != nil {
 		lc.Errorf("failed to create Secret Provider: %v", err)
@@ -64,15 +73,55 @@ func Main(ctx context.Context, cancel context.CancelFunc) {
 	}
 
 	lc.Info("Secret Provider created")
+	var accessToken string
+	var getAccessToken types.GetAccessTokenCallback
 
-	_, err = secretProvider.GetAccessToken("consul", common.CoreCommonConfigServiceKey)
-	if err != nil {
-		lc.Errorf("failed to get Access Token for config provider: %v", err)
-		os.Exit(1)
+	if secretProvider != nil {
+		getAccessToken = func() (string, error) {
+			accessToken, err = secretProvider.GetAccessToken("consul", common.CoreCommonConfigServiceKey)
+			if err != nil {
+				return "", fmt.Errorf("failed to get Configuration Provider access token: %s", err.Error())
+			}
+			return accessToken, err
+		}
+		_, err = getAccessToken()
+		if err != nil {
+			lc.Errorf("failed to get Access Token for config provider: %s", err.Error())
+			os.Exit(1)
+		}
 	}
 
 	lc.Info("Got Config Provider Access Token")
-	lc.Info("Core Common Config Ready for stage two")
+	// create config client
+	envVars := environment.NewVariables(lc)
+	configProviderInfo, err := config.NewProviderInfo(envVars, f.ConfigProviderUrl())
+	if err != nil {
+		lc.Errorf("failed to get Provider Info for the common configuration: %s", err.Error())
+		os.Exit(1)
+	}
+	configClient, err := config.CreateProviderClient(lc, common.CoreCommonConfigServiceKey, common.ConfigStemCore, getAccessToken, configProviderInfo.ServiceConfig())
+	if err != nil {
+		lc.Errorf("failed to create provider client for the common configuration: %s", err.Error())
+		os.Exit(1)
+	}
+	// check to see if the configuration exists in the config provider
+	hasConfig, err := configClient.HasConfiguration()
+	if err != nil {
+		lc.Errorf("failed to determine if configuration exists in the provider: %s", err.Error())
+		os.Exit(1)
+	}
+	lc.Infof("configuration exists: %v", hasConfig)
+	// load the yaml file and push it using the config client
+	if !hasConfig || f.OverwriteConfig() {
+		yamlFile := config.GetConfigLocation(lc, f)
+		lc.Infof("parsing %s for configuration", yamlFile)
+		err = loadYaml(lc, yamlFile, configClient)
+		if err != nil {
+			lc.Error(err.Error())
+			os.Exit(1)
+		}
+	}
+
 	lc.Info("Core Common Config exiting")
 	os.Exit(0)
 }
@@ -98,4 +147,75 @@ func translateInterruptToCancel(ctx context.Context, wg *sync.WaitGroup, cancel 
 			return
 		}
 	}()
+}
+
+func loadYaml(lc logger.LoggingClient, yamlFile string, configClient configuration.Client) error {
+	// push not done flag to configClient
+	err := configClient.PutConfigurationValue(commonConfigDone, []byte("false"))
+	if err != nil {
+		return fmt.Errorf("failed to push %s on startup: %s", commonConfigDone, err.Error())
+	}
+	lc.Infof("reading %s", yamlFile)
+	contents, err := os.ReadFile(yamlFile)
+	if err != nil {
+		return fmt.Errorf("failed to read common configuration file %s: %s", yamlFile, err.Error())
+	}
+
+	data := make(map[string]interface{})
+	kv := make(map[string]interface{})
+
+	err = yaml.Unmarshal(contents, &data)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshall common configuration file %s: %s", yamlFile, err.Error())
+	}
+
+	kv = buildKeyValues(data, kv, "")
+
+	keys := make([]string, 0, len(kv))
+
+	for k := range kv {
+		keys = append(keys, k)
+	}
+
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		v := kv[k]
+		// Push key/value into Consul rather than print
+		err = configClient.PutConfigurationValue(k, []byte(fmt.Sprint(v)))
+		if err != nil {
+			return fmt.Errorf("failed to push common configuration key %s with value %v: %s", k, v, err.Error())
+		}
+	}
+
+	// push done flag to config client
+	err = configClient.PutConfigurationValue(commonConfigDone, []byte("true"))
+	if err != nil {
+		return fmt.Errorf("failed to push %s on completion: %s", commonConfigDone, err.Error())
+	}
+	return nil
+}
+
+// buildKeyValues is a helper function to parse the configuration yaml file
+func buildKeyValues(data map[string]interface{}, kv map[string]interface{}, origKey string) map[string]interface{} {
+	key := origKey
+	for k, v := range data {
+		if len(key) == 0 {
+			key = fmt.Sprintf("%s", k)
+		} else {
+			key = fmt.Sprintf("%s/%s", key, k)
+		}
+
+		vdata, ok := v.(map[string]interface{})
+		if !ok {
+			kv[key] = v
+			key = origKey
+			continue
+		}
+
+		kv = buildKeyValues(vdata, kv, key)
+		key = origKey
+	}
+
+	return kv
 }
