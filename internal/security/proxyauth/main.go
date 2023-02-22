9d9af93b21638de29cd3df6fcfe8fba5ed489a3b
Bryon Nevis <bryon.nevis@intel.com>
2023-02-22 15:18:39 -0800

    feat: Create security-proxy-auth microservice (for proxy swapout)
    This commit introduces code for the security-proxy-auth microservice.
    The proxy-auth microservice will be used by the NGINX reverse proxy
    to perform JWT authentication of inbound requests through the proxy.
    
    This is second in a series of commits to implement the
    microservice token-based authentication ADR.
    
    Signed-off-by: Bryon Nevis <bryon.nevis@intel.com>

---
+++ internal/security/proxyauth/main.go
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * Copyright 2020 Dell Inc.
+ * Copyright 2022 IOTech Ltd.
+ * Copyright 2023 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *******************************************************************************/
+
+package proxyauth
+
+import (
+	"context"
+	"os"
+
+	"github.com/edgexfoundry/go-mod-bootstrap/v3/bootstrap"
+	"github.com/edgexfoundry/go-mod-bootstrap/v3/bootstrap/flags"
+	"github.com/edgexfoundry/go-mod-bootstrap/v3/bootstrap/handlers"
+	"github.com/edgexfoundry/go-mod-bootstrap/v3/bootstrap/interfaces"
+	"github.com/edgexfoundry/go-mod-bootstrap/v3/bootstrap/startup"
+	bootstrapConfig "github.com/edgexfoundry/go-mod-bootstrap/v3/config"
+	"github.com/edgexfoundry/go-mod-bootstrap/v3/di"
+
+	"github.com/edgexfoundry/edgex-go"
+	"github.com/edgexfoundry/edgex-go/internal/security/proxyauth/config"
+	"github.com/edgexfoundry/edgex-go/internal/security/proxyauth/container"
+
+	"github.com/edgexfoundry/go-mod-core-contracts/v3/common"
+
+	"github.com/gorilla/mux"
+)
+
+const SecurityProxyAuthServiceKey = "security-proxy-auth"
+
+func Main(ctx context.Context, cancel context.CancelFunc, router *mux.Router) {
+	startupTimer := startup.NewStartUpTimer(common.CoreCommandServiceKey)
+
+	// All common command-line flags have been moved to DefaultCommonFlags. Service specific flags can be add here,
+	// by inserting service specific flag prior to call to commonFlags.Parse().
+	// Example:
+	// 		flags.FlagSet.StringVar(&myvar, "m", "", "Specify a ....")
+	//      ....
+	//      flags.Parse(os.Args[1:])
+	//
+	f := flags.New()
+	f.Parse(os.Args[1:])
+
+	configuration := &config.ConfigurationStruct{}
+	dic := di.NewContainer(di.ServiceConstructorMap{
+		container.ConfigurationName: func(get di.Get) interface{} {
+			return configuration
+		},
+	})
+
+	httpServer := handlers.NewHttpServer(router, true)
+
+	bootstrap.Run(
+		ctx,
+		cancel,
+		f,
+		SecurityProxyAuthServiceKey,
+		common.ConfigStemSecurity,
+		configuration,
+		startupTimer,
+		dic,
+		true,
+		bootstrapConfig.ServiceTypeOther,
+		[]interfaces.BootstrapHandler{
+			handlers.NewClientsBootstrap().BootstrapHandler,
+			NewBootstrap(router, SecurityProxyAuthServiceKey).BootstrapHandler,
+			httpServer.BootstrapHandler,
+			handlers.NewStartMessage(SecurityProxyAuthServiceKey, edgex.Version).BootstrapHandler,
+		})
+
+	// code here!
+}
