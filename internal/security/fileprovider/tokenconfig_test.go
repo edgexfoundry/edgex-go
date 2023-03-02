a219a96092df6e8a7d6599bd49f1a07919d2084f
Bryon Nevis <bryon.nevis@intel.com>
2023-03-02 13:15:59 -0800

    fix: Typos in Apache-2.0 SPDX headers (#4406)
    Signed-off-by: Bryon Nevis <bryon.nevis@intel.com>

--- internal/security/fileprovider/tokenconfig_test.go
+++ internal/security/fileprovider/tokenconfig_test.go
@@ -10,7 +10,7 @@
 // or implied. See the License for the specific language governing permissions and limitations under
 // the License.
 //
-// SPDX-License-Identifier: Apache-2.0'
+// SPDX-License-Identifier: Apache-2.0
 package fileprovider
 
 import (
