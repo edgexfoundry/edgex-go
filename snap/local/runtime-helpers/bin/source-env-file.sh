47e5a6d2aeb3e105d26e4efa2c2ccb6ce5c919e1
Mengyi Wang <mengyi.wang@canonical.com>
2023-05-03 17:26:49 +0200

    build(snap): Upgrade snap base to core22, upgrade env file loading logic (#4530)
    Signed-off-by: Mengyi Wang <mengyi.wang@canonical.com>

--- snap/local/runtime-helpers/bin/source-env-file.sh
+++ snap/local/runtime-helpers/bin/source-env-file.sh
@@ -8,18 +8,17 @@ BINPATH="${ARGV[0]}"
 
 # binary name == service name/key
 SERVICE=$(basename "$BINPATH")
-if [ -z $ENV_FILE ]; then
-    ENV_FILE="$SNAP_DATA/config/$SERVICE/res/$SERVICE.env"
-fi
+ENV_FILE="$SNAP_DATA/config/$SERVICE/overrides.env"
 TAG="edgex-$SERVICE."$(basename "$0")
 
 if [ -f "$ENV_FILE" ]; then
     logger --tag=$TAG "sourcing $ENV_FILE"
     set -o allexport
     source "$ENV_FILE" set
-    set +o allexport 
+    set +o allexport
 else
     logger --tag=$TAG --stderr "sourcing $ENV_FILE: not found!"
 fi
 
 exec "$@"
+
