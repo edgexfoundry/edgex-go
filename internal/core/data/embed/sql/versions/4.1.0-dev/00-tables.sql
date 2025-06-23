5e4d5c5b2c6b3a00bbfa15e66a9cdddaec124b90
bruce <weichou1229@gmail.com>
2025-06-23 10:57:46 +0800

    feat: Add event_id index on Core Data reading table
    Add the event_id index on reading table to speed up the query.
    
    Close #5176
    
    Signed-off-by: bruce <weichou1229@gmail.com>

--- internal/core/data/embed/sql/versions/4.1.0-dev/00-tables.sql
+++ internal/core/data/embed/sql/versions/4.1.0-dev/00-tables.sql
@@ -4,3 +4,6 @@
 -- SPDX-License-Identifier: Apache-2.0
 
 ALTER TABLE core_data.device_info ADD COLUMN IF NOT EXISTS mark_deleted BOOLEAN DEFAULT false;
+
+-- create index on reading(event_id) to enhance the query performance
+CREATE INDEX IF NOT EXISTS idx_reading_event_id ON core_data.reading(event_id);
