e41795855604c1f2b39b58d9f8fa85a8c70e160d
FelixTing <felix@iotechsys.com>
2025-10-16 14:06:29 +0800

    feat: Add `%` to core-keeper allowed characters regex string
    Signed-off-by: FelixTing <felix@iotechsys.com>

--- internal/core/keeper/utils/key.go
+++ internal/core/keeper/utils/key.go
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2024 IOTech Ltd
+// Copyright (C) 2024-2025 IOTech Ltd
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -14,7 +14,7 @@ import (
 // ValidateKeys validates if the key contains invalid characters
 func ValidateKeys(key string) errors.EdgeX {
 	if !constants.KeyAllowedCharsRegex.MatchString(key) {
-		return errors.NewCommonEdgeX(errors.KindContractInvalid, "key only allows characters between a to z, A to Z, 0 to 9, or one of -_ ~;=./", nil)
+		return errors.NewCommonEdgeX(errors.KindContractInvalid, "key only allows characters between a to z, A to Z, 0 to 9, or one of -_ ~;=./%", nil)
 	}
 	return nil
 }
